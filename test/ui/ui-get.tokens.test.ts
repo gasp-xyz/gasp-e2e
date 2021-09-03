b31a10d648be20b547df1f3ef706c545bb5b7387
goncer <gonza.cerro@gmail.com>
2021-09-03 11:27:01 +0200

    improve faucet check validating by API

--- test/ui/ui-get.tokens.test.ts
+++ test/ui/ui-get.tokens.test.ts
@@ -3,6 +3,7 @@
  * @group ui
  */
 import { Keyring } from "@polkadot/api";
+import BN from "bn.js";
 import { WebDriver } from "selenium-webdriver";
 import { getApi, initApi } from "../../utils/api";
 import { Mangata } from "../../utils/frontend/pages/Mangata";
@@ -11,7 +12,9 @@ import {
   setupAllExtensions,
   takeScreenshot,
 } from "../../utils/frontend/utils/Helper";
-import { User } from "../../utils/User";
+import { getAllAssets } from "../../utils/tx";
+import { AssetWallet, User } from "../../utils/User";
+import { MGA_ASSET_ID } from "../../utils/utils";
 
 jest.setTimeout(1500000);
 jest.spyOn(console, "log").mockImplementation(jest.fn());
@@ -43,6 +46,8 @@ describe("UI tests - Get Tokens from Faucet", () => {
 
     testUser1 = new User(keyring, undefined);
     testUser1.addFromAddress(keyring, polkUserAddress);
+    testUser1.addAsset(MGA_ASSET_ID);
+    await testUser1.refreshAmounts(AssetWallet.BEFORE);
   });
 
   it("As a User I can get test tokens from the faucet", async () => {
@@ -54,6 +59,12 @@ describe("UI tests - Get Tokens from Faucet", () => {
     await mga.waitForFaucetToGenerateTokens();
     const tokens = await mga.getAssetValue();
     expect(tokens).toBe("10.000");
+    await testUser1.refreshAmounts(AssetWallet.AFTER);
+    expect(testUser1.getAsset(MGA_ASSET_ID)?.amountAfter).bnEqual(
+      new BN("10000000000000000000")
+    );
+    const assets = await getAllAssets(testUser1.keyRingPair.address);
+    expect(assets.length).toBeGreaterThanOrEqual(0);
   });
 
   afterEach(async () => {
