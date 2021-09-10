d522e2c25e24eabbee3d20e22092f866f1eeb53a
goncer <gonza.cerro@gmail.com>
2021-09-10 12:15:42 +0200

    increase coverage for faucet test

--- utils/Constants.ts
+++ utils/Constants.ts
@@ -7,3 +7,6 @@ export const ETH_ASSET_NAME = "ETH";
 export const mETH_ASSET_NAME = "mETH";
 export const MGA_DEFAULT_LIQ_TOKEN = new BN(3);
 export const FIVE_MIN = 300000;
+export const DOT_ASSET_NAME = "mDOT";
+export const BTC_ASSET_NAME = "mBTC";
+export const USDC_ASSET_NAME = "mUSDC";
