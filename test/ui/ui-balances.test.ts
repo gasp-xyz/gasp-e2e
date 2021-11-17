b88a2a1400c1cdd38e166537be91e335f00b0557
goncer <gonza.cerro@gmail.com>
2021-11-17 10:38:54 +0100

    add test to validate asset numbers

--- test/ui/ui-balances.test.ts
+++ test/ui/ui-balances.test.ts
@@ -19,6 +19,8 @@ import { AssetWallet, User } from "../../utils/User";
 import { getEnvironmentRequiredVars } from "../../utils/utils";
 import { FIVE_MIN, MAX_BALANCE, MGA_ASSET_NAME } from "../../utils/Constants";
 import { Assets } from "../../utils/Assets";
+import { Pool } from "../../utils/frontend/pages/Pool";
+import { WithdrawModal } from "../../utils/frontend/pages/WithdrawModal";
 
 const MGA_ASSET_ID = new BN(0);
 
@@ -26,7 +28,7 @@ jest.setTimeout(FIVE_MIN);
 jest.spyOn(console, "log").mockImplementation(jest.fn());
 let driver: WebDriver;
 
-describe("UI tests - A user can use small and big numbers:", () => {
+describe("UI tests > Balances", () => {
   let keyring: Keyring;
   let testUser1: User;
   let sudo: User;
@@ -57,6 +59,19 @@ describe("UI tests - A user can use small and big numbers:", () => {
     );
     assetNameMax = Assets.getAssetName(maxValueToken.toString());
     assetNameMin = Assets.getAssetName(minValueToken.toString());
+    const amountToMint = new BN(visibleValueNumber).div(new BN(2000));
+    await testUser1.createPoolToAsset(
+      amountToMint,
+      amountToMint,
+      maxValueToken,
+      MGA_ASSET_ID
+    );
+    await testUser1.createPoolToAsset(
+      new BN(10),
+      amountToMint,
+      minValueToken,
+      MGA_ASSET_ID
+    );
   });
 
   it("As a User I can see the max possible value in UI", async () => {
@@ -73,6 +88,12 @@ describe("UI tests - A user can use small and big numbers:", () => {
     const copiedAssetValue = await sidebar.copyAssetValue(assetNameMax);
     const balanceAvailableInSwap = await swapView.getBalanceFromAssetGet();
 
+    const poolView = new Pool(driver);
+    await poolView.togglePool();
+    await poolView.selectToken1Asset(MGA_ASSET_NAME);
+    await poolView.selectToken2Asset(assetNameMax);
+    const balanceAvailableInPool = await poolView.getBalanceFromtoken2();
+
     // format and expect
     const split18decimals = testUser1
       .getAsset(maxValueToken)
@@ -83,8 +104,10 @@ describe("UI tests - A user can use small and big numbers:", () => {
 
     expect(split18decimals).toEqual(copiedAssetValue);
     expect(split18decimals).toEqual(balanceAvailableInSwap);
+    expect(split18decimals).toEqual(balanceAvailableInPool);
     expect(copiedAssetValue.includes(visibleAssetValue)).toBeTruthy();
   });
+
   it("As a User I can see the min value in UI", async () => {
     await testUser1.refreshAmounts(AssetWallet.BEFORE);
     //do
@@ -100,25 +123,57 @@ describe("UI tests - A user can use small and big numbers:", () => {
     const copiedAssetValue = await sidebar.copyAssetValue(assetNameMin);
     const balanceAvailableInSwap = await swapView.getBalanceFromAssetGet();
 
+    const poolView = new Pool(driver);
+    await poolView.togglePool();
+    await poolView.selectToken1Asset(MGA_ASSET_NAME);
+    await poolView.selectToken2Asset(assetNameMin);
+    const balanceAvailableInPool = await poolView.getBalanceFromtoken2();
+
     // format and expect
     const assetFromNode =
       testUser1.getAsset(minValueToken)?.amountBefore!.free!;
 
     expect(assetFromNode).bnEqual(uiStringToBN(copiedAssetValue));
     expect(assetFromNode).bnEqual(uiStringToBN(balanceAvailableInSwap));
+    expect(assetFromNode).bnEqual(uiStringToBN(balanceAvailableInPool));
     expect(visibleAssetValue).toEqual("<0.001");
   });
+
+  //TODO: Remove skip when we allo users to withdraw specific tokens.
+  it.skip("As a User I can see the min/max when withdrawing", async () => {
+    await testUser1.refreshAmounts(AssetWallet.BEFORE);
+    //do
+    const mga = new Mangata(driver);
+    await mga.navigate();
+    const sidebar = new Sidebar(driver);
+    await sidebar.clickOnWithdrawToEth();
+    const withdrawModal = new WithdrawModal(driver);
+    await withdrawModal.selectToken(assetNameMax);
+    const amountFromWithdrawMax = await withdrawModal.getBalanceAssetAmount();
+    await withdrawModal.selectToken(assetNameMin);
+    const amountFromWithdrawMin = await withdrawModal.getBalanceAssetAmount();
+    await withdrawModal.clickBack();
+
+    const assetFromNodeMin =
+      testUser1.getAsset(minValueToken)?.amountBefore!.free!;
+    const assetFromNodeMax =
+      testUser1.getAsset(maxValueToken)?.amountBefore!.free!;
+
+    expect(assetFromNodeMin).bnEqual(uiStringToBN(amountFromWithdrawMin));
+    expect(assetFromNodeMax).bnEqual(uiStringToBN(amountFromWithdrawMax));
+  });
+
   afterEach(async () => {
     const session = await driver.getSession();
     await addExtraLogs(
       driver,
       expect.getState().currentTestName + " - " + session.getId()
     );
-    await driver.quit();
-    await DriverBuilder.destroy();
   });
 
   afterAll(async () => {
+    await driver.quit();
+    await DriverBuilder.destroy();
     const api = getApi();
     await api.disconnect();
   });
