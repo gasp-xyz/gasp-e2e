f2abed4d23af037dad5be9314387ebe63b7710ee
Eddy <sup@eddy.gg>
2021-11-18 06:11:35 -0800

    Refactor voting test

--- test/governance/voting.users.test.ts
+++ test/governance/voting.users.test.ts
@@ -3,7 +3,6 @@
  */
 
 import BN from "bn.js";
-import { includes } from "lodash";
 import { GovernanceUser } from "../../utils/Framework/User/GovernanceUser";
 import { Keyring } from "@polkadot/api";
 import { Bank } from "../../utils/Framework/Supply/Bank";
@@ -12,13 +11,16 @@ import { SudoUser } from "../../utils/Framework/User/SudoUser";
 import { UserFactory, Users } from "../../utils/Framework/User/UserFactory";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
 import { getEnvironmentRequiredVars, waitForNBlocks } from "../../utils/utils";
-import { testLog } from "../../utils/Logger";
 
 const { chainUri: environmentUri } = getEnvironmentRequiredVars();
 
 let bootnode: Node;
 let keyring: Keyring;
 let sudo: SudoUser;
+
+let candidate: GovernanceUser;
+let voter: GovernanceUser;
+let bank: Bank;
 
 jest.spyOn(console, "log").mockImplementation(jest.fn());
 jest.setTimeout(1500000);
@@ -34,56 +36,58 @@ beforeAll(async () => {
   sudo = UserFactory.createUser(Users.SudoUser, keyring, bootnode) as SudoUser;
 });
 
+beforeEach(async () => {
+  // Initialize actors
+  candidate = UserFactory.createUser(
+    Users.GovernanceUser,
+    keyring,
+    bootnode
+  ) as GovernanceUser;
+
+  voter = UserFactory.createUser(
+    Users.GovernanceUser,
+    keyring,
+    bootnode
+  ) as GovernanceUser;
+
+  bank = new Bank(sudo);
+
+  // Fund accounts
+  await voter.addMGATokens(bank.sudoUser, new BN(Math.pow(10, 17).toString()));
+  await candidate.addMGATokens(
+    bank.sudoUser,
+    new BN(Math.pow(10, 16).toString())
+  );
+
+  // Subscribe to events
+  candidate.node.subscribeToUserBalanceChanges(candidate);
+  voter.node.subscribeToUserBalanceChanges(voter);
+});
+
 describe("Governance -> Voting -> Users", () => {
-  test("Users can vote for a new council", async () => {
-    const candidate = UserFactory.createUser(
-      Users.GovernanceUser,
-      keyring,
-      bootnode
-    ) as GovernanceUser;
+  test("Users can vote for a new council member", async () => {
+    const stake = new BN(Math.pow(10, 16).toString());
 
-    const voter = UserFactory.createUser(
-      Users.GovernanceUser,
-      keyring,
-      bootnode
-    ) as GovernanceUser;
-
-    const bank = new Bank(sudo);
-
-    await voter.addMGATokens(
-      bank.sudoUser,
-      new BN(Math.pow(10, 17).toString())
-    );
-    await candidate.addMGATokens(
-      bank.sudoUser,
-      new BN(Math.pow(10, 16).toString())
-    );
-    candidate.node.subscribeToUserBalanceChanges(candidate);
-    voter.node.subscribeToUserBalanceChanges(voter);
-
+    // Gonzalo runs for council
+    // Eddy votes for Gonzalo
     await candidate.runForCouncil();
-    await voter.vote([candidate], new BN(Math.pow(10, 16).toString()));
+    await voter.vote([candidate], stake);
     await waitForNBlocks(2);
-    const electionInfo = bootnode.electionEvents.get(bootnode.lastBlock! - 1);
-    testLog.getLog().info(`Test Info:
-    Candidates:  ${electionInfo?.candidates}
-    Looking for: ${candidate.keyRingPair.address}`);
-    expect(
-      includes(electionInfo?.candidates, candidate.keyRingPair.address)
-    ).toBe(true);
-    // Check uer wallets.
-    // TODO: Adjust and assert user wallets.
-    const [voterBefore, voterAfter] = [
-      voter.node.userBalancesHistory.get(
-        voter.node.userBalancesHistory.keys().next().value
-      ),
-      voter.node.userBalancesHistory.get(bootnode.lastBlock! - 1),
-    ];
-    testLog.getLog().debug(`UserWallet Info:
-      Before:  ${JSON.stringify(voterBefore?.get(0))}
-      After: ${JSON.stringify(voterAfter?.get(0))}`);
-    expect(new BN(Math.pow(10, 16).toString())).bnEqual(
-      voterAfter?.get(0)?.reserved!
+
+    // Gonzalo is now listed in the latest blocks candidates list
+    const electionCandidates = bootnode.electionEvents.get(
+      bootnode.lastBlock! - 1
+    )!.candidates;
+
+    expect(electionCandidates).toEqual(
+      expect.arrayContaining([candidate.keyRingPair.address])
     );
+
+    // Eddy has reserved the voting stake amount in his last transaction
+    const votersRecentlyReservedTx = voter.node.userBalancesHistory
+      .get(bootnode.lastBlock! - 1)!
+      .get(0)!.reserved!;
+
+    expect(stake).bnEqual(votersRecentlyReservedTx);
   });
 });
