c372eaf321426959bc030d30e684dc732814de8b
goncer <gonza.cerro@gmail.com>
2022-04-22 17:04:31 +0200

    add types for console app  and smal html for visualization

--- testPad/calculateRewards.ts
+++ testPad/calculateRewards.ts
@@ -1,19 +1,10 @@
 /* eslint-disable no-console */
-//README:
-//Configure uris constant.
-//Build And Run!
-// npx ts-node test/exploratory/eventListener.ts
-
+import { options } from "@mangata-finance/types";
 import BN from "bn.js";
+import fs from "fs";
 import { ApiPromise } from "@polkadot/api";
-//import { KeyringPair } from "@polkadot/keyring/types";
 import { WsProvider } from "@polkadot/rpc-provider/ws";
-//import { Mangata } from "mangata-sdk";
-import type { DefinitionRpc, DefinitionRpcSub } from "@polkadot/types/types";
-const { testLog } = require("./../utils/Logger");
-
-//this constant will skip some traces.
-// this will handle if printing in pretty-multilines.
+import { testLog } from "../utils/Logger";
 
 async function main() {
   const users = [
@@ -21,43 +12,28 @@ async function main() {
     "5FfBQ3kwXrbdyoqLPvcXRp7ikWydXawpNs2Ceu3WwFdhZ8W4",
   ];
   const provider = new WsProvider("ws://10.0.0.6:9944");
-  const rpcOptions: Record<
-    string,
-    Record<string, DefinitionRpc | DefinitionRpcSub>
-  > = {
-    xyk: {
-      calculate_rewards_amount: {
-        description: "",
-        params: [
-          {
-            name: "user",
-            type: "AccountId",
-          },
-          {
-            name: "liquidity_asset_id",
-            type: "u32",
-          },
-        ],
-        type: "RpcResult<Balance>",
-      },
-    },
-  };
-  const api = await new ApiPromise({
-    provider,
-    rpc: rpcOptions,
-  }).isReady;
-  await api.connect();
-  const asd = await api.isConnected;
-  //const mangata = Mangata.getInstance("ws://10.0.0.6:9944");
-  //const api = await mangata.getApi();
-  console.log(asd);
-  users.forEach((user) => {
-    (api.rpc as any).xyk
-      .calculate_rewards_amount(user, new BN(5))
-      .then((result: any) => {
-        console.info(`${Date.now}` + (result as any).toString());
-      });
+  const api = await new ApiPromise(options({ provider })).isReady;
+  await api.rpc.chain.subscribeNewHeads((header) => {
+    users.forEach((user) => {
+      (api.rpc as any).xyk
+        .calculate_rewards_amount(user, new BN(5))
+        .then((result: any) => {
+          const str = `${user}:${header.number}:${(
+            result as any
+          ).notYetClaimed.toString()}:${(
+            result as any
+          ).toBeClaimed.toString()}`;
+          const plott = `${header.number},${(
+            result as any
+          ).notYetClaimed.toString()} \n`;
+          fs.appendFile(`${user}.txt`, plott, function (err) {
+            if (err) throw err;
+            console.log(str);
+          });
+        });
+    });
   });
+  //await api.disconnect();
 }
 
 main().catch((error) => {
