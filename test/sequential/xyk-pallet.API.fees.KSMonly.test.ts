b4e21177945f2fd2680888e1ede77650dc072d5c
Aleksandr Chetverov <105198474+AlexChetverov@users.noreply.github.com>
2022-07-21 13:55:04 +0300

    Add files via upload
    Add files for checking fees in KSM and MGX tokens

---
+++ test/sequential/xyk-pallet.API.fees.KSMonly.test.ts
@@ -0,0 +1,111 @@
+/*
+ *
+ * @group xyk
+ * @group accuracy
+ * @group parallel
+ */
+import { getApi, getMangataInstance, initApi } from "../../utils/api";
+import { ExtrinsicResult } from "../../utils/eventListeners";
+import { BN } from "@polkadot/util";
+import { Keyring } from "@polkadot/api";
+import { AssetWallet, User } from "../../utils/User";
+import { Assets } from "../../utils/Assets";
+import { getEnvironmentRequiredVars } from "../../utils/utils";
+import { getEventResultFromMangataTx } from "../../utils/txHandler";
+import { KSM_ASSET_ID } from "../../utils/Constants";
+import { toBN } from "@mangata-finance/sdk";
+
+jest.spyOn(console, "log").mockImplementation(jest.fn());
+jest.spyOn(console, "error").mockImplementation(jest.fn());
+jest.setTimeout(1500000);
+process.env.NODE_ENV = "test";
+
+let testUser1: User;
+let sudo: User;
+
+let keyring: Keyring;
+let firstCurrency: BN;
+let secondCurrency: BN;
+const first_asset_amount = new BN(50000);
+const second_asset_amount = new BN(50000);
+//creating pool
+
+const { sudo: sudoUserName } = getEnvironmentRequiredVars();
+
+const defaultCurrecyValue = new BN(250000);
+
+beforeEach(async () => {
+  try {
+    getApi();
+  } catch (e) {
+    await initApi();
+  }
+
+  keyring = new Keyring({ type: "sr25519" });
+
+  // setup users
+  testUser1 = new User(keyring);
+  sudo = new User(keyring, sudoUserName);
+
+  //add two curerncies and balance to testUser:
+  [firstCurrency, secondCurrency] = await Assets.setupUserWithCurrencies(
+    testUser1,
+    [defaultCurrecyValue, defaultCurrecyValue],
+    sudo
+  );
+
+  //add KSM tokens.
+  await sudo.mint(
+    KSM_ASSET_ID,
+    testUser1,
+    toBN ('1', 17)
+  );
+
+  // add users to pair.
+  keyring.addPair(testUser1.keyRingPair);
+  keyring.addPair(sudo.keyRingPair);
+  testUser1.addAsset(KSM_ASSET_ID);
+
+});
+
+test("xyk-pallet - Calculate required MGA fee - CreatePool and BuyAsset", async () => {
+
+  await (await getMangataInstance())
+    .createPool(
+      testUser1.keyRingPair,
+      firstCurrency.toString(),
+      first_asset_amount,
+      secondCurrency.toString(),
+      second_asset_amount
+    )
+    .then((result) => {
+      const eventResponse = getEventResultFromMangataTx(result);
+      expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
+    });
+
+  await testUser1.refreshAmounts(AssetWallet.BEFORE);
+
+  await (await getMangataInstance())
+    .buyAsset(
+      testUser1.keyRingPair,
+      firstCurrency.toString(),
+      secondCurrency.toString(),
+      new BN(100),
+      new BN(1000000)
+    )
+    .then((result) => {
+      const eventResponse = getEventResultFromMangataTx(result);
+      expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
+    });
+});
+
+afterEach(async () => {
+  await testUser1.refreshAmounts(AssetWallet.AFTER);
+  const deductedKSMTkns = testUser1
+    .getAsset(KSM_ASSET_ID)
+    ?.amountBefore.free.sub(
+      testUser1.getAsset(KSM_ASSET_ID)?.amountAfter.free!
+    );
+
+  expect(deductedKSMTkns).bnGt(new BN(0));
+});
---
+++ test/sequential/xyk-pallet.API.fees.someMGX.test.ts
@@ -0,0 +1,147 @@
+/*
+ *
+ * @group xyk
+ * @group accuracy
+ * @group parallel
+ */
+import { getApi, getMangataInstance, initApi } from "../../utils/api";
+import { getCurrentNonce } from "../../utils/tx";
+import { ExtrinsicResult } from "../../utils/eventListeners";
+import { BN } from "@polkadot/util";
+import { Keyring } from "@polkadot/api";
+import { AssetWallet, User } from "../../utils/User";
+import { Assets } from "../../utils/Assets";
+import { getEnvironmentRequiredVars } from "../../utils/utils";
+import { SignerOptions } from "@polkadot/api/types";
+import { getEventResultFromMangataTx } from "../../utils/txHandler";
+import { RuntimeDispatchInfo } from "@polkadot/types/interfaces";
+import { MGA_ASSET_ID, KSM_ASSET_ID } from "../../utils/Constants";
+import { toBN } from "@mangata-finance/sdk";
+
+jest.spyOn(console, "log").mockImplementation(jest.fn());
+jest.spyOn(console, "error").mockImplementation(jest.fn());
+jest.setTimeout(1500000);
+process.env.NODE_ENV = "test";
+
+let testUser1: User;
+let sudo: User;
+
+let keyring: Keyring;
+let firstCurrency: BN;
+let secondCurrency: BN;
+const first_asset_amount = new BN(50000);
+const second_asset_amount = new BN(50000);
+//creating pool
+
+const { sudo: sudoUserName } = getEnvironmentRequiredVars();
+
+let cost: RuntimeDispatchInfo;
+
+const defaultCurrecyValue = new BN(250000);
+
+beforeEach(async () => {
+  try {
+    getApi();
+  } catch (e) {
+    await initApi();
+  }
+
+  keyring = new Keyring({ type: "sr25519" });
+
+  // setup users
+  testUser1 = new User(keyring);
+  sudo = new User(keyring, sudoUserName);
+
+  //add two curerncies and balance to testUser:
+  [firstCurrency, secondCurrency] = await Assets.setupUserWithCurrencies(
+    testUser1,
+    [defaultCurrecyValue, defaultCurrecyValue],
+    sudo
+  );
+
+  //add some MGX tokens.
+
+  await sudo.mint(
+    MGA_ASSET_ID,
+    testUser1,
+    toBN ('29', 18)
+  );
+
+  //add few KSM tokens.
+  await sudo.mint(
+    KSM_ASSET_ID,
+    testUser1,
+    new BN(9000000000)
+  );
+
+  // add users to pair.
+  keyring.addPair(testUser1.keyRingPair);
+  keyring.addPair(sudo.keyRingPair);
+  testUser1.addAsset(MGA_ASSET_ID);
+  testUser1.addAsset(KSM_ASSET_ID);
+
+});
+
+test("xyk-pallet - Calculate required MGA fee - CreatePool and BuyAsset", async () => {
+  const api = getApi();
+  const nonce = await getCurrentNonce(testUser1.keyRingPair.address);
+  const opt: Partial<SignerOptions> = {
+    nonce: nonce,
+    tip: 0,
+  };
+  cost = await api?.tx.xyk
+    .buyAsset(
+    firstCurrency.toString(),
+    secondCurrency.toString(),
+    new BN(100),
+    new BN(1000000)
+    )
+    .paymentInfo(testUser1.keyRingPair, opt);
+
+  await (await getMangataInstance())
+    .createPool(
+      testUser1.keyRingPair,
+      firstCurrency.toString(),
+      first_asset_amount,
+      secondCurrency.toString(),
+      second_asset_amount
+    )
+    .then((result) => {
+      const eventResponse = getEventResultFromMangataTx(result);
+      expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
+    });
+
+  await testUser1.refreshAmounts(AssetWallet.BEFORE);
+
+  await (await getMangataInstance())
+    .buyAsset(
+      testUser1.keyRingPair,
+      firstCurrency.toString(),
+      secondCurrency.toString(),
+      new BN(100),
+      new BN(1000000)
+    )
+    .then((result) => {
+      const eventResponse = getEventResultFromMangataTx(result);
+      expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
+    });
+});
+
+afterEach(async () => {
+  await testUser1.refreshAmounts(AssetWallet.AFTER);
+  const deductedMGATkns = testUser1
+    .getAsset(MGA_ASSET_ID)
+    ?.amountBefore.free.sub(
+      testUser1.getAsset(MGA_ASSET_ID)?.amountAfter.free!
+    );
+  const deductedKSMTkns = testUser1
+    .getAsset(KSM_ASSET_ID)
+    ?.amountBefore.free.sub(
+      testUser1.getAsset(KSM_ASSET_ID)?.amountAfter.free!
+    );
+   
+  const fee = cost.partialFee;
+
+  expect(deductedMGATkns).bnLte(fee);
+  expect(deductedKSMTkns).bnEqual(new BN(0));
+});
