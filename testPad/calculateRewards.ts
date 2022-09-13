9f481ca2d37a2e1024dabbbb550857376793d4a3
Goncer <gonza.cerro@gmail.com>
2022-09-13 11:45:53 +0200

    add test for testing burntokens

--- testPad/calculateRewards.ts
+++ testPad/calculateRewards.ts
@@ -6,54 +6,47 @@ import { getMangataInstance } from "../utils/api";
 
 async function main() {
   const users = [
-    "5FA3LcCrKMgr9WHqyvtDhDarAXRkJjoYrSy6XnZPKfwiB3sY",
-    "5FfBQ3kwXrbdyoqLPvcXRp7ikWydXawpNs2Ceu3WwFdhZ8W4",
-    "5FRL15Qj6DdoULKswCz7zevqe97bnHuEix794pTeGK7MhfDS",
-    "5H6YCgW24Z8xJDvxytQnKTwgiJGgye3uqvfQTprBEYqhNbBy",
+    "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y",
+    "5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy",
+    "5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",
+    "5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL",
   ];
-  const liq = process.env.liq ? process.env.liq : 16;
+  const liq = process.env.liq ? process.env.liq : 8;
   const liqId = new BN(liq);
   const mangata = await getMangataInstance("ws://127.0.0.1:8844");
   // const provider = new WsProvider("ws://127.0.0.1:8844");
   //const api = await new ApiPromise(options({ provider })).isReady;
   const api = await mangata.getApi();
+  api.query.issuance.promotedPoolsRewardsV2(liqId).then((value) => {
+    console.log(`${JSON.stringify(value.toHuman())}`);
+  });
   await api.rpc.chain.subscribeNewHeads((header) => {
-    api.query.xyk.rewardsInfo.entries().then((value) => {
-      value.forEach((value) => {
-        console.log(`${value[0].toHuman()} - ${value[1].toHuman()}`);
-      });
-    });
-    api.query.issuance.promotedPoolsRewardsV2.entries().then((value) => {
-      value.forEach((value) => {
-        console.log(`${value[0].toHuman()} - ${value[1].toHuman()}`);
+    users.forEach((user) => {
+      api.query.xyk.rewardsInfo(user, liqId).then((value) => {
+        console.log(`RW_info: ${JSON.stringify(value.toHuman())}}`);
       });
     });
     users.forEach((user) => {
       mangata
         .calculateRewardsAmountV2(user, liqId.toString())
         .then((result: any) => {
-          console.log("foo: " + liq + "-" + JSON.stringify(result));
+          if (result.toString() === "0")
+            console.log("foo: " + liq + "-" + JSON.stringify(result));
           if (result.toString() !== "0") {
             const str = `${user}:${header.number}:${(
               result as any
-            ).notYetClaimed.toString()}:${(
-              result as any
-            ).toBeClaimed.toString()}`;
+            ).toString()}`;
 
-            const plott = `${header.number},${(
-              result as any
-            ).notYetClaimed.toString()},${(
-              result as any
-            ).toBeClaimed.toString()} \n`;
+            const plott = `${header.number},${(result as any).toString()} \n`;
 
             fs.appendFile(
-              `/home/goncer/projects/${liqId}_${user}.txt`,
+              `/home/goncer/projects/mangata-e2e/${liqId}_${user}.txt`,
               plott,
               function (err) {
                 if (err) throw err;
-                console.log("ohoh" + err + str);
               }
             );
+            console.log(str);
           }
         });
     });
