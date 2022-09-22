0f416a36c11efecad23689ffe79c939ca9e4c3e8
Goncer <gonza.cerro@gmail.com>
2022-09-22 09:41:20 +0200

    add some aux functionalities for manual testing

--- test/channels/asset_register.test.ts
+++ test/channels/asset_register.test.ts
@@ -4,9 +4,10 @@ import { User } from "../../utils/User";
 import { Keyring } from "@polkadot/api";
 import { AcalaNode } from "../../utils/Framework/Node/AcalaNode";
 import { Utils } from "./Utils";
-import { beforeAll, test } from "vitest";
 
 const { acalaUri } = getEnvironmentRequiredVars();
+jest.setTimeout(1500000);
+jest.spyOn(console, "log").mockImplementation(jest.fn());
 
 let alice: User;
 let acala: AcalaNode;
@@ -34,6 +35,7 @@ test("asset register - register MGR on Acala", async () => {
 });
 
 test("asset register - register LKSM on Mangata", async () => {
+  //@ts-ignore
   const tx = api!.tx.assetRegistry.registerAsset(
     Utils.assetLocation(2000, "0x0083")
     //    {
@@ -45,5 +47,34 @@ test("asset register - register LKSM on Mangata", async () => {
     //    },
     //    null
   );
+  await Utils.signAndSend(alice, api!.tx.sudo.sudo(tx));
+});
+
+test("register LKSM", async () => {
+  const tx = api!.tx.assetRegistry.registerAsset(
+    {
+      decimals: 12,
+      name: "KAR- 0x0083",
+      symbol: "LKSM",
+      existentialDeposit: 0,
+      location: {
+        V1: {
+          parents: 1,
+          interior: {
+            X2: [
+              {
+                Parachain: 2000,
+              },
+              {
+                GeneralKey: "0x0083",
+              },
+            ],
+          },
+        },
+      },
+    },
+    9
+  );
+
   await Utils.signAndSend(alice, api!.tx.sudo.sudo(tx));
 });
--- test/channels/xTokens-Karura-rococo.test.ts
+++ test/channels/xTokens-Karura-rococo.test.ts
@@ -2,40 +2,51 @@ import { getEnvironmentRequiredVars } from "../../utils/utils";
 import { User } from "../../utils/User";
 import { AcalaNode } from "../../utils/Framework/Node/AcalaNode";
 import { Utils } from "./Utils";
-import { beforeAll, test } from "vitest";
 import { Keyring } from "@polkadot/api";
 import { Node } from "../../utils/cluster/Node";
+import { BN } from "@mangata-finance/sdk";
 
 let { acalaUri } = getEnvironmentRequiredVars();
 const KAR_WEIGHT = 4 * 200_000_000;
 const MG_WEIGHT = 4 * 1_000_000_000;
 let mga: Node;
 let testUser: any;
+let keyring: Keyring;
 let alice: User;
 let acala: AcalaNode;
-
+jest.setTimeout(1500000);
 beforeAll(async () => {
   //await initApi();
-  acalaUri = "wss://karura-rococo.aca-dev.network";
+  acalaUri = "ws://127.0.0.1:9946";
   acala = new AcalaNode(acalaUri);
-  mga = new Node(
-    "mga",
-    "wss://roccoco-testnet-collator-01.mangatafinance.cloud"
-  );
+  mga = new Node("mga", "ws://127.0.0.1:9948");
   await acala.connect();
   await mga.connect();
 
-  const keyring = new Keyring({ type: "sr25519" });
+  keyring = new Keyring({ type: "sr25519" });
   alice = new User(keyring, "//Alice");
-  testUser = new User(keyring, "foo");
-  testUser.addFromMnemonic(keyring, "<mnemonic>");
+  testUser = new User(keyring, "//Eve");
+  //  testUser.addFromMnemonic(
+  //    keyring,
+  //    "dismiss addict reduce fitness install aisle creek they seek palace stereo trumpet"
+  //  );
 });
 
-test("transfer KAR to Mangata", async () => {
+test("1transfer KAR to Mangata", async () => {
   const tx = acala.api!.tx.xTokens.transfer(
-    { Token: "KSM" },
+    { Token: "KAR" },
     Utils.amount(10, 12),
-    Utils.location(2110, testUser.publicKey),
+    Utils.location(2110, testUser.keyRingPair.publicKey),
+    MG_WEIGHT
+  );
+  await Utils.signAndSend(alice, tx);
+});
+
+test("1transfer LKSM to Mangata", async () => {
+  const tx = acala.api!.tx.xTokens.transfer(
+    { Token: "LKSM" },
+    Utils.amount(10, 12),
+    Utils.location(2110, testUser.keyRingPair.publicKey),
     MG_WEIGHT
   );
   await Utils.signAndSend(alice, tx);
@@ -43,8 +54,8 @@ test("transfer KAR to Mangata", async () => {
 
 test("transfer KAR to Mangata-MA", async () => {
   const tx = acala.api!.tx.xTokens.transferMultiasset(
-    Utils.asset(2000, "0x0080", Utils.amount(10, 12)),
-    Utils.location(2110, testUser.publicKey),
+    Utils.asset(2000, "0x0080", Utils.amount(10, 15)),
+    Utils.location(2110, testUser.keyRingPair.publicKey),
     MG_WEIGHT
   );
   await Utils.signAndSend(alice, tx);
@@ -95,4 +106,118 @@ test("transfer - transfer multiasset MGR back to Mangata", async () => {
     MG_WEIGHT
   );
   await Utils.signAndSend(alice, tx);
+});
+test("configure asset registry", async () => {
+  const tx = mga.api!.tx.assetRegistry.updateAsset(
+    6,
+    12,
+    "Karura",
+    "KAR",
+    0,
+    {
+      V1: {
+        parents: 1,
+        interior: {
+          X2: [
+            {
+              Parachain: 2000,
+            },
+            {
+              GeneralKey: "0x0080",
+            },
+          ],
+        },
+      },
+    },
+    {
+      xcm: {
+        feePerSecond: 53760000000001,
+      },
+    }
+  );
+
+  await Utils.signAndSend(alice, mga.api!.tx.sudo.sudo(tx));
+});
+test("remove fee/s", async () => {
+  const tx = mga.api!.tx.assetRegistry.updateAsset(
+    6,
+    12,
+    "Karura",
+    "KAR",
+    0,
+    {
+      V1: {
+        parents: 1,
+        interior: {
+          X2: [
+            {
+              Parachain: 2000,
+            },
+            {
+              GeneralKey: "0x0080",
+            },
+          ],
+        },
+      },
+    },
+    null
+  );
+
+  await Utils.signAndSend(alice, mga.api!.tx.sudo.sudo(tx));
+});
+test("update - LKSM add 1 fee", async () => {
+  const tx = mga.api!.tx.assetRegistry.updateAsset(
+    8,
+    12,
+    "KAR - 0x0083",
+    "LKSM",
+    0,
+    {
+      V1: {
+        parents: 1,
+        interior: {
+          X2: [
+            {
+              Parachain: 2000,
+            },
+            {
+              GeneralKey: "0x0083",
+            },
+          ],
+        },
+      },
+    },
+    {
+      xcm: {
+        feePerSecond: 10000,
+      },
+    }
+  );
+
+  await Utils.signAndSend(alice, mga.api!.tx.sudo.sudo(tx));
+});
+
+test("send cuatrillion txs", async () => {
+  let currNonce = new BN(
+    (
+      (await acala.api?.query.system.account(alice.keyRingPair.address)) as any
+    ).nonce
+  );
+  acala = new AcalaNode("ws://127.0.0.1:9946");
+  await acala.connect();
+  const promises = [];
+  for (let i = 0; i < 1000; i++) {
+    const tx = acala.api!.tx.xTokens.transfer(
+      { Token: "KAR" },
+      Utils.amount(10, 12),
+      Utils.location(2110, testUser.keyRingPair.publicKey),
+      MG_WEIGHT
+    );
+    const signed = tx.sign(alice.keyRingPair, { nonce: currNonce });
+    const p = signed.send();
+    promises.push(p);
+    currNonce = currNonce.addn(1);
+    //await Utils.signAndSend(alice, tx);
+  }
+  await Promise.all(promises);
 });
