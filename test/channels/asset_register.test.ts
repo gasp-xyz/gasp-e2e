0a49f10824fbc25570b6e387974d00472a433043
Goncer <gonza.cerro@gmail.com>
2022-09-26 12:38:10 +0200

    add test to reproduce bug

--- test/channels/asset_register.test.ts
+++ test/channels/asset_register.test.ts
@@ -4,6 +4,8 @@ import { User } from "../../utils/User";
 import { Keyring } from "@polkadot/api";
 import { AcalaNode } from "../../utils/Framework/Node/AcalaNode";
 import { Utils } from "./Utils";
+import { testLog } from "../../utils/Logger";
+import { BN } from "bn.js";
 
 const { acalaUri } = getEnvironmentRequiredVars();
 jest.setTimeout(1500000);
@@ -34,7 +36,7 @@ test("asset register - register MGR on Acala", async () => {
   await Utils.signAndSend(alice, acala.api!.tx.sudo.sudo(tx));
 });
 
-test("asset register - register LKSM on Mangata", async () => {
+test.skip("asset register - register LKSM on Mangata", async () => {
   //@ts-ignore
   const tx = api!.tx.assetRegistry.registerAsset(
     Utils.assetLocation(2000, "0x0083")
@@ -66,15 +68,150 @@ test("register LKSM", async () => {
                 Parachain: 2000,
               },
               {
-                GeneralKey: "0x0083",
+                GeneralKey: "0x0080",
               },
             ],
           },
         },
       },
     },
-    9
+    8
   );
 
   await Utils.signAndSend(alice, api!.tx.sudo.sudo(tx));
+});
+test("register incorrect", async () => {
+  const tx = api!.tx.assetRegistry.registerAsset(
+    {
+      decimals: 12,
+      name: "KAR- 0x0083",
+      symbol: "LKSM",
+      existentialDeposit: 0,
+      location: {
+        V1: {
+          parents: 1,
+          interior: {
+            X3: [
+              {
+                Parachain: 2000,
+              },
+              {
+                GeneralKey: "0x00080",
+              },
+              {
+                PalletInstance: 50,
+              },
+            ],
+          },
+        },
+      },
+    },
+    8
+  );
+
+  await Utils.signAndSend(alice, api!.tx.sudo.sudo(tx));
+});
+
+test("get 2000, 0x0080", async () => {
+  const asset = await api!.query.assetRegistry.locationToAssetId({
+    parents: 1,
+    interior: {
+      X2: [
+        {
+          Parachain: 2000,
+        },
+        {
+          GeneralKey: "0x0080",
+        },
+      ],
+    },
+  });
+  testLog.getLog().warn(asset.toHuman());
+
+  const asset2 = await api!.query.assetRegistry.locationToAssetId({
+    parents: 1,
+    interior: {
+      X2: [
+        {
+          Parachain: 2000,
+        },
+        {
+          GeneralKey: "0x00080",
+        },
+        {
+          PalletInstance: 50,
+        },
+      ],
+    },
+  });
+  testLog.getLog().warn(asset2.toHuman());
+});
+
+
+test("reproduceBug", async () => {
+  const generatedAsset = true;
+  if(!generatedAsset){
+    const tx = api!.tx.assetRegistry.registerAsset(
+      {
+        decimals: 12,
+        name: "KAR- 0x0083",
+        symbol: "LKSM",
+        existentialDeposit: 0,
+        location: {
+          V1: {
+            parents: 1,
+            interior: {
+              X3: [
+                {
+                  Parachain: 2000,
+                },
+                {
+                  GeneralKey: "0x00080",
+                },
+                {
+                  PalletInstance: 50,
+                },
+              ],
+            },
+          },
+        },
+      },
+      8
+    );
+    await Utils.signAndSend(alice, api!.tx.sudo.sudo(tx));
+  }
+  const asset = await api!.query.assetRegistry.locationToAssetId({
+    parents: 1,
+    interior: {
+      X2: [
+        {
+          Parachain: 2000,
+        },
+        {
+          GeneralKey: "0x0080",
+        },
+      ],
+    },
+  });
+  testLog.getLog().warn(asset.toHuman());
+
+  const asset2 = await api!.query.assetRegistry.locationToAssetId({
+    parents: 1,
+    interior: {
+      X2: [
+        {
+          Parachain: 2000,
+        },
+        {
+          GeneralKey: "0x00080",
+        },
+        {
+          PalletInstance: 50,
+        },
+      ],
+    },
+  });
+  testLog.getLog().warn(asset2.toHuman());
+  expect(new BN(asset.toString())).bnEqual(new BN(6));
+  expect(new BN(asset2.toString())).bnEqual(new BN(8));
 });
