6b599fe3db9a833a1ddbd959a0a5156567612f98
MariuSZ <112616777+MariuszSzpyt@users.noreply.github.com>
2022-09-27 15:36:50 +0200

    test(UI): make UI tests great again (#160)
    * test(UI): make UI tests great again
    
    * formatting
    
    * ESlint fix
    
    * PR fixes

--- test/ui/ui-main.extensions.test.ts
+++ test/ui/ui-main.extensions.test.ts
@@ -8,14 +8,13 @@ import { Mangata } from "../../utils/frontend/pages/Mangata";
 import { Sidebar } from "../../utils/frontend/pages/Sidebar";
 import { DriverBuilder } from "../../utils/frontend/utils/Driver";
 import {
-  setupAllExtensions,
+  acceptPermissionsPolkadotExtension,
   addExtraLogs,
-  openInNewTab,
-  swithToTheOtherTab,
+  setupPolkadotExtension,
 } from "../../utils/frontend/utils/Helper";
 
 import { FIVE_MIN } from "../../utils/Constants";
-import { Polkadot } from "../../utils/frontend/pages/Polkadot";
+import { WalletConnectModal } from "../../utils/frontend/pages/WalletConnectModal";
 
 jest.setTimeout(FIVE_MIN);
 jest.spyOn(console, "log").mockImplementation(jest.fn());
@@ -32,141 +31,51 @@ describe("UI tests - Extension management", () => {
     }
   });
 
-  it("As a User I get infomed whenever is neccesary to install any extension", async () => {
+  it("User is informed about required extensions", async () => {
     driver = await DriverBuilder.getInstance(false);
 
     const mga = new Mangata(driver);
     await mga.go();
     const sidebar = new Sidebar(driver);
-    await sidebar.waitForLoad();
-    const metaDiv = await sidebar.isMetamaskExtensionNotFoundDisplayed();
-    const polkDiv = await sidebar.isPolkExtensionNotFoundDisplayed();
-    const metaBtnInstall = await sidebar.isMetamaskInstallBtnDisplayed();
-    const polkBtnInstall = await sidebar.isPolkInstallBtnDisplayed();
-    expect(metaDiv).toBeTruthy();
-    expect(polkDiv).toBeTruthy();
-    expect(metaBtnInstall).toBeTruthy();
-    expect(polkBtnInstall).toBeTruthy();
+    const noWalletConnectedInfoDisplayed =
+      await sidebar.isNoWalletConnectedInfoDisplayed();
+    expect(noWalletConnectedInfoDisplayed).toBeTruthy();
+
+    await sidebar.clickOnWalletConnect();
+    const walletConnectModal = new WalletConnectModal(driver);
+    const isWalletConnectModalDisplayed = await walletConnectModal.opens();
+    expect(isWalletConnectModalDisplayed).toBeTruthy();
+    await walletConnectModal.pickWallet("Polkadot");
+    const isReqExtensionInfoDisplayed =
+      await walletConnectModal.isReqExtensionInfoDisplayed("Polkadot");
+    expect(isReqExtensionInfoDisplayed).toBeTruthy();
   });
 
-  it("As a User I get feedback when extensions are installed and correctly setup", async () => {
+  it("User can connect polkadot.js extension with accounts", async () => {
     driver = await DriverBuilder.getInstance();
-    await setupAllExtensions(driver);
+    await setupPolkadotExtension(driver);
 
     const mga = new Mangata(driver);
     await mga.go();
     const sidebar = new Sidebar(driver);
-    await sidebar.waitForLoad();
-    const metaDiv = await sidebar.isMetamaskExtensionNotFoundDisplayed();
-    const polkDiv = await sidebar.isPolkExtensionNotFoundDisplayed();
-    const metaBtnInstall = await sidebar.isMetamaskInstallBtnDisplayed();
-    const polkBtnInstall = await sidebar.isPolkInstallBtnDisplayed();
-    expect(metaDiv).toBeFalsy();
-    expect(polkDiv).toBeFalsy();
-    expect(metaBtnInstall).toBeFalsy();
-    expect(polkBtnInstall).toBeFalsy();
+    const noWalletConnectedInfoDisplayed =
+      await sidebar.isNoWalletConnectedInfoDisplayed();
+    expect(noWalletConnectedInfoDisplayed).toBeTruthy();
 
-    const isMetaOK = await sidebar.isMetamaskExtensionOK();
-    const ispolkOK = await sidebar.isPolkadotExtensionOK();
-
-    expect(isMetaOK).toBeTruthy();
-    expect(ispolkOK).toBeTruthy();
-  });
-
-  it("As a User I can switch between polkadot wallets", async () => {
-    driver = await DriverBuilder.getInstance();
-    const polkUserAddress = await (
-      await setupAllExtensions(driver)
-    ).polkUserAddress;
-    const mga = new Mangata(driver);
+    await sidebar.clickOnWalletConnect();
+    const walletConnectModal = new WalletConnectModal(driver);
+    const isWalletConnectModalDisplayed = await walletConnectModal.opens();
+    expect(isWalletConnectModalDisplayed).toBeTruthy();
+    await walletConnectModal.pickWallet("Polkadot");
+    await acceptPermissionsPolkadotExtension(driver);
     await mga.go();
-    const sidebar = new Sidebar(driver);
-    await sidebar.waitForLoad();
-    const isMetaOK = await sidebar.isMetamaskExtensionOK();
-    let ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(isMetaOK).toBeTruthy();
-    expect(ispolkOK).toBeTruthy();
-
-    const extensionManager = new Polkadot(driver);
-    await openInNewTab(driver, extensionManager.WEB_UI_ACCESS_URL);
-    const [polkUserAddress2] = await extensionManager.createAccount(1);
-    await swithToTheOtherTab(driver);
-    let userSelected = await sidebar.getUserName();
-    expect(userSelected).toEqual("acc_automation");
-    ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(ispolkOK).toBeTruthy();
-
-    await swithToTheOtherTab(driver);
-    await extensionManager.hideAccount(polkUserAddress);
-    await swithToTheOtherTab(driver);
-    userSelected = await sidebar.getUserName();
-    expect(userSelected).toEqual("acc_automation_1");
-    ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(ispolkOK).toBeTruthy();
-
-    await swithToTheOtherTab(driver);
-    await extensionManager.hideAccount(polkUserAddress2);
-    await swithToTheOtherTab(driver);
-
-    ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(ispolkOK).toBeFalsy();
-  });
-  it("As a User I can switch between polkadot wallets using the modal", async () => {
-    driver = await DriverBuilder.getInstance();
-    await (
-      await setupAllExtensions(driver)
-    ).polkUserAddress;
-    const mga = new Mangata(driver);
-    await mga.go();
-    const sidebar = new Sidebar(driver);
-    await sidebar.waitForLoad();
-    const isMetaOK = await sidebar.isMetamaskExtensionOK();
-    let ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(isMetaOK).toBeTruthy();
-    expect(ispolkOK).toBeTruthy();
-
-    const extensionManager = new Polkadot(driver);
-    await openInNewTab(driver, extensionManager.WEB_UI_ACCESS_URL);
-    const [polkUserAddress2] = await extensionManager.createAccount(1);
-    await swithToTheOtherTab(driver);
-    let userSelected = await sidebar.getUserName();
-    expect(userSelected).toEqual("acc_automation");
-    ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(ispolkOK).toBeTruthy();
-
-    await sidebar.switchAccountTo(polkUserAddress2);
-    userSelected = await sidebar.getUserName();
-    expect(userSelected).toEqual("acc_automation_1");
-    ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(ispolkOK).toBeTruthy();
-  });
-  it("As a User I can not see hidden wallets in the modal", async () => {
-    driver = await DriverBuilder.getInstance();
-    const firstUserAddress = await (
-      await setupAllExtensions(driver)
-    ).polkUserAddress;
-    const mga = new Mangata(driver);
-    await mga.go();
-    const sidebar = new Sidebar(driver);
-    await sidebar.waitForLoad();
-    const isMetaOK = await sidebar.isMetamaskExtensionOK();
-    let ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(isMetaOK).toBeTruthy();
-    expect(ispolkOK).toBeTruthy();
-    const accountsBefore = await sidebar.getAvailableAccountsFromChangeModal();
-    expect(accountsBefore).toEqual([firstUserAddress]);
-
-    const extensionManager = new Polkadot(driver);
-    await openInNewTab(driver, extensionManager.WEB_UI_ACCESS_URL);
-    const [secondUserAddress] = await extensionManager.createAccount(1);
-    await extensionManager.hideAccount(firstUserAddress);
-    await extensionManager.hideAccount(secondUserAddress);
-    await swithToTheOtherTab(driver);
-    ispolkOK = await sidebar.isPolkadotExtensionOK();
-    expect(ispolkOK).toBeFalsy();
-
-    const accounts = await sidebar.getAvailableAccountsFromChangeModal();
-    expect(accounts.length).toEqual(0);
+    await sidebar.clickOnWalletConnect();
+    await walletConnectModal.pickWallet("Polkadot");
+    await walletConnectModal.pickAccount("acc_automation");
+    const isWalletConnected = sidebar.isWalletConnected("acc_automation");
+    expect(isWalletConnected).toBeTruthy();
+    const areSidebarElementsVisible = sidebar.areSidebarElementsVisible();
+    expect(areSidebarElementsVisible).toBeTruthy();
   });
 
   afterEach(async () => {
