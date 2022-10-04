17c1f257b2c3b2d8b6c41ce67ad76698938cd161
Aleks Chetverov <alexchetverov@gmail.com>
2022-10-04 17:38:16 +0100

    temp

--- test/ci/xyk-ci.validate.node.test.ts
+++ test/ci/xyk-ci.validate.node.test.ts
@@ -6,32 +6,32 @@
  */
 import { getApi, initApi } from "../../utils/api";
 import { testLog } from "../../utils/Logger";
-// import { Header } from "@polkadot/types/interfaces/runtime";
+import { Header } from "@polkadot/types/interfaces/runtime";
 
 jest.spyOn(console, "log").mockImplementation(jest.fn());
 jest.setTimeout(1500000);
 process.env.NODE_ENV = "test";
 // const DEFAULT_TIME_OUT_MS = 42000;
 
-// async function waitNewHeaders(numHeads = 5): Promise<Header[]> {
-//   return new Promise(async (resolve, reject) => {
-//     setTimeout(() => {
-//       reject();
-//     }, DEFAULT_TIME_OUT_MS);
+async function waitNewHeaders(numHeads = 5): Promise<Header[]> {
+  return new Promise(async (resolve, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 80000);
 
-//     const api = getApi();
-//     let count = 0;
-//     const blocks: Header[] = [];
-//     // Subscribe to the new headers
-//     const unsubHeads = await api.rpc.chain.subscribeNewHeads((lastHeader) => {
-//       blocks.push(lastHeader);
-//       if (++count === numHeads) {
-//         unsubHeads();
-//         resolve(blocks);
-//       }
-//     });
-//   });
-// }
+    const api = getApi();
+    let count = 0;
+    const blocks: Header[] = [];
+    // Subscribe to the new headers
+    const unsubHeads = await api.rpc.chain.subscribeNewHeads((lastHeader) => {
+      blocks.push(lastHeader);
+      if (++count === numHeads) {
+        unsubHeads();
+        resolve(blocks);
+      }
+    });
+  });
+}
 
 beforeAll(async () => {
   try {
@@ -52,11 +52,11 @@ test("xyk-CI - Node is up and running", async () => {
   testLog.getLog().info("Node version : " + version.toString());
   expect(version.toString()).not.toBeUndefined();
 
-  // const heads = await waitNewHeaders(2);
-  // const [headNo1, headNo0] = [
-  //   JSON.parse(heads[1].toString()).number,
-  //   JSON.parse(heads[0].toString()).number,
-  // ];
-  // testLog.getLog().info(`Node numbers : #${headNo0} , #${headNo1}`);
-  // expect(headNo1).toBeGreaterThan(headNo0);
+  const heads = await waitNewHeaders(2);
+  const [headNo1, headNo0] = [
+    JSON.parse(heads[1].toString()).number,
+    JSON.parse(heads[0].toString()).number,
+  ];
+  testLog.getLog().info(`Node numbers : #${headNo0} , #${headNo1}`);
+  expect(headNo1).toBeGreaterThan(headNo0);
 });
