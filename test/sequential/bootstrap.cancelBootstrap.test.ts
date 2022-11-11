d464cb633c17acbbf95acc9363456eb4a447954c
AlexChetverov <alexchetverov@gmail.com>
2022-11-11 16:53:42 +0100

    update test functions

--- test/sequential/bootstrap.cancelBootstrap.test.ts
+++ test/sequential/bootstrap.cancelBootstrap.test.ts
@@ -10,23 +10,20 @@ import {
   finalizeBootstrap,
   cancelRunningBootstrap,
 } from "../../utils/tx";
-import { EventResult, ExtrinsicResult } from "../../utils/eventListeners";
 import { Keyring } from "@polkadot/api";
 import { User } from "../../utils/User";
+import {
+  createNewBootstrapToken,
+  setupBootstrapTokensBalance,
+} from "../../utils/Bootstrap";
 import {
   getEnvironmentRequiredVars,
   waitForBootstrapStatus,
 } from "../../utils/utils";
-import {
-  getEventResultFromMangataTx,
-  sudoIssueAsset,
-} from "../../utils/txHandler";
+import { getEventErrorfromSudo } from "../../utils/txHandler";
 import { MGA_ASSET_ID } from "../../utils/Constants";
-import { BN, MangataGenericEvent, toBN } from "@mangata-finance/sdk";
-import { hexToU8a } from "@polkadot/util";
-import { Assets } from "../../utils/Assets";
-import { Sudo } from "../../utils/sudo";
-import { setupApi, setupUsers } from "../../utils/setup";
+import { MangataGenericEvent } from "@mangata-finance/sdk";
+import { setupUsers } from "../../utils/setup";
 
 jest.spyOn(console, "log").mockImplementation(jest.fn());
 jest.spyOn(console, "error").mockImplementation(jest.fn());
@@ -38,15 +35,19 @@ let sudo: User;
 let keyring: Keyring;
 let bootstrapPhase: any;
 let bootstrapCurrency: any;
-let eventResponse: EventResult;
+let cancelBootstrapEvent: MangataGenericEvent[];
 
 const { sudo: sudoUserName } = getEnvironmentRequiredVars();
-const waitingPeriod1 = 400;
-const waitingPeriod2 = 15;
+//constant for bootstrap include a planning period
+const waitingPeriodWithPlan = 400;
+//constant for bootstrap less a planning period
+const waitingPeriodLessPlan = 15;
 const bootstrapPeriod = 30;
 const whitelistPeriod = 10;
 
-async function checkBootstrapEvent(checkingEvent: MangataGenericEvent[]) {
+async function checkSudoOperataionSuccess(
+  checkingEvent: MangataGenericEvent[]
+) {
   const filterBootstrapEvent = checkingEvent.filter(
     (extrinsicResult) => extrinsicResult.method === "Sudid"
   );
@@ -56,31 +57,17 @@ async function checkBootstrapEvent(checkingEvent: MangataGenericEvent[]) {
   expect(userBootstrapCall).toContain("Ok");
 }
 
-async function checkCancellingBootstrapError(
-  sudoUser: User,
+async function checkSudoOperataionFail(
+  checkingEvent: MangataGenericEvent[],
   expectedError: string
 ) {
-  const api = getApi();
-  const checkingCancelling = await cancelRunningBootstrap(sudoUser);
-  const filterBootstrapEvent = checkingCancelling.filter(
+  const filterBootstrapEvent = checkingEvent.filter(
     (extrinsicResult) => extrinsicResult.method === "Sudid"
   );
 
-  const userBootstrapErr = hexToU8a(
-    //@ts-ignore
-    filterBootstrapEvent[0].event.data[0].asErr.value.error.toString()
-  );
+  const BootstrapError = await getEventErrorfromSudo(filterBootstrapEvent);
 
-  const userBootstrapIndex =
-    //@ts-ignore
-    filterBootstrapEvent[0].event.data[0].asErr.value.index.toString();
-
-  const userAssetMetaError = api?.registry.findMetaError({
-    error: userBootstrapErr,
-    index: new BN(userBootstrapIndex),
-  });
-
-  expect(userAssetMetaError.method).toContain(expectedError);
+  expect(BootstrapError.method).toContain(expectedError);
 }
 
 beforeAll(async () => {
@@ -96,41 +83,13 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-  const api = getApi();
-  await setupApi();
-
   testUser1 = new User(keyring);
 
   [testUser1] = setupUsers();
 
-  const bootstrapCurrencyIssue = await sudoIssueAsset(
-    sudo.keyRingPair,
-    toBN("1", 20),
-    sudo.keyRingPair.address
-  );
-  const bootstrapEventResult = await getEventResultFromMangataTx(
-    bootstrapCurrencyIssue,
-    ["tokens", "Issued", sudo.keyRingPair.address]
-  );
-  const bootstrapAssetId = bootstrapEventResult.data[0].split(",").join("");
-  bootstrapCurrency = new BN(bootstrapAssetId);
+  bootstrapCurrency = await createNewBootstrapToken(sudo);
 
-  // check that system is ready to bootstrap
-  bootstrapPhase = await api.query.bootstrap.phase();
-  if (bootstrapPhase.toString() === "Finished") {
-    const bootstrapFinalize = await finalizeBootstrap(sudo);
-    eventResponse = getEventResultFromMangataTx(bootstrapFinalize);
-    expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
-  }
-
-  bootstrapPhase = await api.query.bootstrap.phase();
-  expect(bootstrapPhase.toString()).toEqual("BeforeStart");
-
-  await Sudo.batchAsSudoFinalized(
-    Assets.mintToken(bootstrapCurrency, testUser1),
-    Assets.mintToken(bootstrapCurrency, sudo),
-    Assets.mintNative(testUser1)
-  );
+  await setupBootstrapTokensBalance(bootstrapCurrency, sudo, testUser1);
 });
 
 test("bootstrap - Check that we can cancel bootstrap before planned", async () => {
@@ -138,14 +97,14 @@ test("bootstrap - Check that we can cancel bootstrap before planned", async () =
     sudo,
     MGA_ASSET_ID,
     bootstrapCurrency,
-    waitingPeriod1,
+    waitingPeriodWithPlan,
     bootstrapPeriod,
     whitelistPeriod
   );
-  await checkBootstrapEvent(scheduleBootstrapEvent);
+  await checkSudoOperataionSuccess(scheduleBootstrapEvent);
 
-  const checkingCancelling = await cancelRunningBootstrap(sudo);
-  await checkBootstrapEvent(checkingCancelling);
+  cancelBootstrapEvent = await cancelRunningBootstrap(sudo);
+  await checkSudoOperataionSuccess(cancelBootstrapEvent);
 });
 
 test("bootstrap - Check that we can not cancel bootstrap when bootstrap event already planned or started", async () => {
@@ -153,31 +112,38 @@ test("bootstrap - Check that we can not cancel bootstrap when bootstrap event al
     sudo,
     MGA_ASSET_ID,
     bootstrapCurrency,
-    waitingPeriod2,
+    waitingPeriodLessPlan,
     bootstrapPeriod,
     whitelistPeriod
   );
-  await checkBootstrapEvent(scheduleBootstrapEvent);
+  await checkSudoOperataionSuccess(scheduleBootstrapEvent);
 
-  //check that bootstrap cannot be canceled less than 300 blocks before the start
-  await checkCancellingBootstrapError(sudo, "TooLateToUpdateBootstrap");
+  //check cthat bootstrap cannot be canceled less than 300 blocks before the start
+  cancelBootstrapEvent = await cancelRunningBootstrap(sudo);
+  await checkSudoOperataionFail(
+    cancelBootstrapEvent,
+    "TooLateToUpdateBootstrap"
+  );
 
-  await waitForBootstrapStatus("Whitelist", waitingPeriod2);
+  await waitForBootstrapStatus("Whitelist", waitingPeriodLessPlan);
 
   //check that bootstrap cannot be canceled after the start
-  await checkCancellingBootstrapError(sudo, "AlreadyStarted");
+  cancelBootstrapEvent = await cancelRunningBootstrap(sudo);
+  await checkSudoOperataionFail(cancelBootstrapEvent, "AlreadyStarted");
 
-  await waitForBootstrapStatus("Public", waitingPeriod2);
+  await waitForBootstrapStatus("Public", waitingPeriodLessPlan);
 
-  await checkCancellingBootstrapError(sudo, "AlreadyStarted");
+  cancelBootstrapEvent = await cancelRunningBootstrap(sudo);
+  await checkSudoOperataionFail(cancelBootstrapEvent, "AlreadyStarted");
 
   await waitForBootstrapStatus("Finished", bootstrapPeriod);
 
-  await checkCancellingBootstrapError(sudo, "AlreadyStarted");
+  cancelBootstrapEvent = await cancelRunningBootstrap(sudo);
+  await checkSudoOperataionFail(cancelBootstrapEvent, "AlreadyStarted");
 
   // finalaze bootstrap
   const bootstrapFinalize = await finalizeBootstrap(sudo);
-  await checkBootstrapEvent(bootstrapFinalize);
+  await checkSudoOperataionSuccess(bootstrapFinalize);
 });
 
 afterEach(async () => {
