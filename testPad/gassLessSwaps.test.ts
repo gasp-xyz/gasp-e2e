951663a0c1e5d8ccd84f54ba73918f11edf9bebb
GonCer <gonza.cerro@gmail.com>
2023-01-11 18:45:39 +0100

    add a few more functionalities to testPad

--- testPad/gassLessSwaps.test.ts
+++ testPad/gassLessSwaps.test.ts
@@ -5,19 +5,15 @@ import { getApi, initApi } from "../utils/api";
 import { MGA_ASSET_ID } from "../utils/Constants";
 import { User } from "../utils/User";
 import {
-  getEnvironmentRequiredVars,
-} from "../utils/utils";
-import {
-    getCurrentNonce,
-  getNextAssetId,
+    getNextAssetId,
   getUserAssets,
-  sellAsset,
 } from "../utils/tx";
 import { setupApi, setupUsers } from "../utils/setup";
 import { Assets } from "../utils/Assets";
 import { Sudo } from "../utils/sudo";
 import { Xyk } from "../utils/xyk";
 import { testLog } from "../utils/Logger";
+import { buyAsset } from '../utils/tx';
 
 require("dotenv").config();
 
@@ -35,7 +31,7 @@ describe("staking - testpad", () => {
     }
   });
 
-  test("Create pool and mint tokens to users", async () => {
+  test("gassless Create pool and mint tokens to users", async () => {
     await setupApi();
     await setupUsers();
     const keyring = new Keyring({ type: "sr25519" });
@@ -61,53 +57,50 @@ describe("staking - testpad", () => {
       )
     );
   });
+  test("disable token", async () => {
+    await setupApi();
+    await setupUsers();
+    await Sudo.asSudoFinalized( Assets.updateAsset(new BN(8), {
+      metadata: { xcm: undefined, xyk: { operationsDisabled: false } },
+    })
+    )
+
+  });
   test("Do swap and print balances", async () => {
     await setupApi();
     await setupUsers();
     const keyring = new Keyring({ type: "sr25519" });
-    const charlie = new User(keyring, "//Charlie");
-    //    const eve = new User(keyring, "//Eve");
-    //    const ferdie = new User(keyring, "//Ferdie");
-    const tokenId = new BN(8);
+    const charlie = new User(keyring, "//Ferdie");
+    const tokenId = new BN(11);
+    const tokenId2 = new BN(8);
     const tokensBefore = await getUserAssets(charlie.keyRingPair.address, [
-      new BN(0),
+      tokenId2,
       tokenId,
     ]);
     testLog.getLog().info(JSON.stringify(tokensBefore));
     const promises: any[] = [];
-    const nonce = await getCurrentNonce(charlie.keyRingPair.address)
-    promises.push(sellAsset(
+    //const nonce = await getCurrentNonce(charlie.keyRingPair.address)
+    promises.push(buyAsset(
       charlie.keyRingPair,
-      new BN(0),
+      tokenId2,
       tokenId,
-      new BN(10000),
-      new BN(1), {
-        nonce: nonce
-      }
+      new BN("1000"),
+      new BN("100000")
     ));
-    promises.push(sellAsset(
-        charlie.keyRingPair,
-        new BN(0),
-        tokenId,
-        new BN(10000),
-        new BN(1), {
-          nonce: nonce.addn(1)
-        }
-      ));
     await Promise.all(promises);
 
     const tokensAfter = await getUserAssets(charlie.keyRingPair.address, [
-      new BN(0),
+      tokenId2,
       tokenId,
     ]);
     testLog.getLog().info(JSON.stringify(tokensBefore));
     testLog.getLog().info(JSON.stringify(tokensAfter));
-    testLog.getLog().info("MGA::::  Free:    " + tokensAfter[0].free.sub(tokensBefore[0].free).toNumber() );
-    testLog.getLog().info("MGA::::  Reserved:" + tokensAfter[0].reserved.sub(tokensBefore[0].reserved).toNumber() );
-    testLog.getLog().info("MGA::::  Frozen:  " + tokensAfter[0].frozen.sub(tokensBefore[0].frozen).toNumber() );
+    testLog.getLog().info("MGA::::  Free:    " + tokensAfter[0].free.sub(tokensBefore[0].free).toString() );
+    testLog.getLog().info("MGA::::  Reserved:" + tokensAfter[0].reserved.sub(tokensBefore[0].reserved).toString() );
+    testLog.getLog().info("MGA::::  Frozen:  " + tokensAfter[0].frozen.sub(tokensBefore[0].frozen).toString() );
 
-    testLog.getLog().info("Token::::  Free:  " + tokensAfter[1].free.sub(tokensBefore[1].free).toNumber() );
-    testLog.getLog().info("Token::  Reserved:" + tokensAfter[1].reserved.sub(tokensBefore[1].reserved).toNumber() );
-    testLog.getLog().info("token:::  Frozen: " + tokensAfter[1].frozen.sub(tokensBefore[1].frozen).toNumber() );
+    testLog.getLog().info("Token::::  Free:  " + tokensAfter[1].free.sub(tokensBefore[1].free).toString() );
+    testLog.getLog().info("Token::  Reserved:" + tokensAfter[1].reserved.sub(tokensBefore[1].reserved).toString() );
+    testLog.getLog().info("token:::  Frozen: " + tokensAfter[1].frozen.sub(tokensBefore[1].frozen).toString() );
   });
 });
