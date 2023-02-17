9d1bba2272e3ad0c9467b7132c2646a9157f999e
AlexChetverov <alexchetverov@gmail.com>
2023-02-17 15:01:39 +0100

    updated tests

--- test/story/poolLiquidityRewards.test.ts
+++ test/story/poolLiquidityRewards.test.ts
@@ -4,11 +4,16 @@
  * @group story
  */
 
-import { getApi, initApi } from "../../utils/api";
+import { getApi, getMangataInstance, initApi } from "../../utils/api";
 import { User } from "../../utils/User";
 import { Keyring } from "@polkadot/api";
 import { BN } from "@polkadot/util";
-import { getEnvironmentRequiredVars } from "../../utils/utils";
+import {
+  getEnvironmentRequiredVars,
+  waitBlockNumber,
+  waitForNBlocks,
+  xykErrors,
+} from "../../utils/utils";
 import { Assets } from "../../utils/Assets";
 import { Sudo } from "../../utils/sudo";
 import { Xyk } from "../../utils/xyk";
@@ -18,21 +23,21 @@ import {
   getLiquidityAssetId,
   promotePool,
   deactivateLiquidity,
+  burnLiquidity,
 } from "../../utils/tx";
 import { setupApi, setupUsers } from "../../utils/setup";
-import { ExtrinsicResult } from "../../utils/eventListeners";
+import { ExtrinsicResult, waitForRewards } from "../../utils/eventListeners";
+import { MGA_ASSET_ID } from "../../utils/Constants";
 
-const { sudo: sudoUserName } = getEnvironmentRequiredVars();
 const defaultCurrencyValue = new BN(10000000);
-const defaultPoolVolumeValue = new BN(1000000);
-const assetAmount = new BN(10000);
+const assetAmount = new BN("1000000000000000");
 
 let testUser1: User;
 let sudo: User;
 
 let keyring: Keyring;
-let firstCurrency: BN;
 let secondCurrency: BN;
+let liqId: BN;
 
 beforeAll(async () => {
   try {
@@ -42,81 +47,101 @@ beforeAll(async () => {
   }
 
   keyring = new Keyring({ type: "sr25519" });
-
-  // setup users
-  sudo = new User(keyring, sudoUserName);
+  sudo = new User(keyring, getEnvironmentRequiredVars().sudo);
 });
 
 beforeEach(async () => {
   [testUser1] = setupUsers();
 
-  [firstCurrency, secondCurrency] = await Assets.setupUserWithCurrencies(
+  secondCurrency = await Assets.issueAssetToUser(
     sudo,
-    [defaultCurrencyValue, defaultCurrencyValue],
-    sudo
+    defaultCurrencyValue,
+    sudo,
+    true
   );
 
   await setupApi();
 
   await Sudo.batchAsSudoFinalized(
-    Assets.mintToken(firstCurrency, testUser1, defaultCurrencyValue),
-    Assets.mintToken(secondCurrency, testUser1, defaultCurrencyValue),
+    Assets.FinalizeTge(),
+    Assets.initIssuance(),
+    Assets.mintToken(secondCurrency, testUser1, Assets.DEFAULT_AMOUNT),
     Assets.mintNative(testUser1),
     Sudo.sudoAs(
-      sudo,
+      testUser1,
       Xyk.createPool(
-        firstCurrency,
-        defaultPoolVolumeValue,
+        MGA_ASSET_ID,
+        Assets.DEFAULT_AMOUNT.divn(2),
         secondCurrency,
-        defaultPoolVolumeValue
+        Assets.DEFAULT_AMOUNT.divn(2)
       )
     )
   );
+
+  liqId = await getLiquidityAssetId(MGA_ASSET_ID, secondCurrency);
 });
 
 test("Given a user with Liquidity on non promoted pool When tries to activate Then extrinsic fail", async () => {
   await mintLiquidity(
     testUser1.keyRingPair,
-    firstCurrency,
+    MGA_ASSET_ID,
     secondCurrency,
     assetAmount
   );
 
-  const liquidityAssetId = await getLiquidityAssetId(
-    firstCurrency,
-    secondCurrency
+  await activateLiquidity(testUser1.keyRingPair, liqId, assetAmount).then(
+    (result) => {
+      expect(result.state).toEqual(ExtrinsicResult.ExtrinsicFailed);
+      expect(result.data).toEqual("NotAPromotedPool");
+    }
   );
-
-  await activateLiquidity(
-    testUser1.keyRingPair,
-    liquidityAssetId,
-    assetAmount
-  ).then((result) => {
-    expect(result.state).toEqual(ExtrinsicResult.ExtrinsicFailed);
-    expect(result.data).toEqual("NotAPromotedPool");
-  });
 });
 
 test("Given a user with Liquidity activated When tries to deactivate Then the user gets all tokens burn and rewards amount are readable in RPC THEN the user can claim them", async () => {
-  const liquidityAssetId = await getLiquidityAssetId(
-    firstCurrency,
-    secondCurrency
+  const { chainUri } = getEnvironmentRequiredVars();
+  const mangata = await getMangataInstance(chainUri);
+
+  await Sudo.batchAsSudoFinalized(
+    Assets.promotePool(liqId.toNumber(), 20),
+    Sudo.sudoAs(
+      testUser1,
+      Xyk.mintLiquidity(MGA_ASSET_ID, secondCurrency, assetAmount)
+    )
   );
 
-  await promotePool(sudo.keyRingPair, liquidityAssetId);
+  await waitForRewards(testUser1, liqId);
 
-  await mintLiquidity(
+  await activateLiquidity(testUser1.keyRingPair, liqId, assetAmount);
+
+  const liqBalance = await mangata.getTokenBalance(
+    liqId.toString(),
+    testUser1.keyRingPair.address
+  );
+
+  await burnLiquidity(
     testUser1.keyRingPair,
-    firstCurrency,
+    MGA_ASSET_ID,
     secondCurrency,
-    assetAmount
+    new BN(liqBalance.free.toString())
   );
 
-  await activateLiquidity(testUser1.keyRingPair, liquidityAssetId, assetAmount);
+  const availableRewardsBefore = await mangata.calculateRewardsAmount(
+    testUser1.keyRingPair.address,
+    liqId.toString()
+  );
 
-  await deactivateLiquidity(
+  await mangata.claimRewards(
     testUser1.keyRingPair,
-    liquidityAssetId,
-    assetAmount
+    liqId.toString(),
+    availableRewardsBefore
   );
+
+  await waitForRewards(testUser1, liqId);
+
+  const availableRewardsAfter = await mangata.calculateRewardsAmount(
+    testUser1.keyRingPair.address,
+    liqId.toString()
+  );
+
+  expect(availableRewardsAfter).bnGt(availableRewardsBefore);
 });
