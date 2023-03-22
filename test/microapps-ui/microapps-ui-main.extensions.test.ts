e5c6ae44aefe7011790328169d492e0ba1526792
MariuSZ <112616777+MariuszSzpyt@users.noreply.github.com>
2023-03-22 14:03:08 +0100

    Microapps polkadotjs wallet test

--- test/microapps-ui/microapps-ui-main.extensions.test.ts
+++ test/microapps-ui/microapps-ui-main.extensions.test.ts
@@ -6,32 +6,80 @@ import { WebDriver } from "selenium-webdriver";
 import { getApi, initApi } from "../../utils/api";
 import { Mangata } from "../../utils/frontend/pages/Mangata";
 import { DriverBuilder } from "../../utils/frontend/utils/Driver";
-import { addExtraLogs } from "../../utils/frontend/utils/Helper";
-
+import {
+  acceptPermissionsPolkadotExtensionInNewWindow,
+  addExtraLogs,
+  setupPolkadotExtension,
+} from "../../utils/frontend/utils/Helper";
+import { Keyring } from "@polkadot/api";
 import { FIVE_MIN } from "../../utils/Constants";
-import { Sidebar } from "../../utils/frontend/microapps-pages/Sidebar";
+import { Main } from "../../utils/frontend/microapps-pages/Main";
+import { User } from "../../utils/User";
+import { setupUsers } from "../../utils/setup";
+import { WalletConnectModal } from "../../utils/frontend/microapps-pages/WalletConnectModal";
+import { WalletWrapper } from "../../utils/frontend/microapps-pages/WalletWrapper";
 
 jest.setTimeout(FIVE_MIN);
 jest.spyOn(console, "log").mockImplementation(jest.fn());
 let driver: WebDriver;
+let testUser1: User;
+const acc_name = "acc_automation";
 
-describe("Miocroapps UI smoke tests", () => {
+describe("Wallets management", () => {
   beforeEach(async () => {
     try {
       getApi();
     } catch (e) {
       await initApi();
     }
+    driver = await DriverBuilder.getInstance();
+    const keyring = new Keyring({ type: "sr25519" });
+    setupUsers();
+    const { mnemonic } = await setupPolkadotExtension(driver);
+    testUser1 = new User(keyring);
+    testUser1.addFromMnemonic(keyring, mnemonic);
   });
 
-  it("App is starting", async () => {
-    driver = await DriverBuilder.getInstance(false);
-
+  it("User can connect Polkadot wallet", async () => {
     const mga = new Mangata(driver);
     await mga.go();
-    const sidebar = new Sidebar(driver);
-    const appLoaded = await sidebar.isAppDisplayed();
+    const walletWrapper = new WalletWrapper(driver);
+    const mainPage = new Main(driver);
+    const appLoaded = await mainPage.isAppLoaded();
     expect(appLoaded).toBeTruthy();
+
+    const isWalletButton = await walletWrapper.isWalletConnectButtonDisplayed();
+    expect(isWalletButton).toBeTruthy();
+
+    await walletWrapper.openWalletConnectionInfo();
+    let isWalletConnected = await walletWrapper.isWalletConnected();
+    expect(isWalletConnected).toBeFalsy();
+
+    await walletWrapper.clickWalletConnect();
+    await walletWrapper.pickWallet("Polkadot");
+
+    const walletConnectModal = new WalletConnectModal(driver);
+    let isWalletConnectModalDisplayed = await walletConnectModal.displayed();
+    expect(isWalletConnectModalDisplayed).toBeTruthy();
+    await acceptPermissionsPolkadotExtensionInNewWindow(driver);
+
+    const areAccountsDisplayed = await walletConnectModal.accountsDisplayed();
+    expect(areAccountsDisplayed).toBeTruthy();
+
+    await walletConnectModal.pickAccount(acc_name);
+    isWalletConnectModalDisplayed = await walletConnectModal.displayed();
+    expect(isWalletConnectModalDisplayed).toBeFalsy();
+
+    isWalletConnected = await walletWrapper.isWalletConnected();
+    expect(isWalletConnected).toBeTruthy();
+
+    const isSuccessToastDisplayed = await mainPage.isToastDisplayed(
+      "Wallet Connected"
+    );
+    expect(isSuccessToastDisplayed).toBeTruthy();
+
+    const isAccInfoDisplayed = await walletWrapper.isAccInfoDisplayed(acc_name);
+    expect(isAccInfoDisplayed).toBeTruthy();
   });
 
   afterEach(async () => {
