c81186000f5a0fed1892f873e2e97fea458ad946
GonCer <gonza.cerro@gmail.com>
2023-03-28 12:01:50 +0200

    add start stop remove docker image

--- testPad/foo.test.ts
+++ testPad/foo.test.ts
@@ -2,12 +2,17 @@
 import { getApi, initApi } from "../utils/api";
 import Docker from "dockerode";
 import execSh from "exec-sh";
+import { sleep } from "../utils/utils";
 require("dotenv").config();
 
 jest.spyOn(console, "log").mockImplementation(jest.fn());
 
 jest.setTimeout(1500000);
 process.env.NODE_ENV = "test";
+let docker;
+const dockerImageName = "test_image:automation";
+const mgaParachainImageName = "parachain-2110";
+const mgaDockerContainerImage = "output-parachain-2110-1";
 
 describe("staking - testpad", () => {
   beforeAll(async () => {
@@ -17,70 +22,81 @@ describe("staking - testpad", () => {
       await initApi();
     }
   });
-  let child;
   test.skip("docker thingy", async () => {
-    const docker = new Docker();
+    docker = new Docker();
     const as = await docker.listContainers();
-    const aliceImage = as.filter(
+    const bobImage = as.filter(
       (x) =>
         x.Command.toLowerCase().includes("bob") &&
-        x.Image.includes("parachain-2110")
+        x.Image.includes(mgaParachainImageName)
     )[0];
-    // const nw = docker.getNetwork(aliceImage.Id);
-    await execSh("docker tag output-parachain-2110-1 foo:brum");
-    const command = aliceImage.Command;
+    await execSh(`docker tag ${mgaDockerContainerImage} ${dockerImageName}`);
+    await sleep(2000);
+    const command = bobImage.Command;
     const commandReady = command
       .replace("/mangata/node", "")
       .replace("--bob", "");
-    // const sanitized = commandReady.split(" ").filter((x) => x.length !== 0);
-    child = execSh(
-      [
-        "docker run  -p 30400:30333 -p 9950:9944 -p 9953:9933 --network=output_default foo:brum" +
-          commandReady,
-      ],
-      true,
-      (err, stdout, stderr) => {
-        console.info("error: ", err);
-        console.info("stdout: ", stdout);
-        console.info("stderr: ", stderr);
-      }
-    );
-    /***
-    const p = docker.run("foo:brum", sanitized, process.stdout, {
-      name: "my-python-container",
-      ExposedPorts: {
-        "9615/tcp": {},
-        "30334/tcp": {},
-        "9944": {},
-        "9933": {},
-        "30333": {},
-      },
-      HostConfig: {
-        AutoRemove: true,
-        Network: "output_default",
-        PortBindings: {
-          "9944": [
-            {
-              HostPort: "0",
-            },
-          ],
-          "9933": [
-            {
-              HostPort: "0", //Map container to a random unused port.
-            },
-          ],
-          "30333": [
-            {
-              HostPort: "0", //Map container to a random unused port.
-            },
-          ],
+    const sanitized = commandReady.split(" ").filter((x) => x.length !== 0);
+    docker.run(
+      dockerImageName,
+      sanitized,
+      process.stdout,
+      {
+        name: "testImage_mangata_node",
+        ExposedPorts: {
+          "9615/tcp": {},
+          "30334/tcp": {},
+          "9944": {},
+          "9933": {},
+          "30333": {},
+        },
+        HostConfig: {
+          AutoRemove: true,
+          NetworkMode: "output_default",
+          PortBindings: {
+            "9944": [
+              {
+                HostPort: "0",
+              },
+            ],
+            "9933": [
+              {
+                HostPort: "0", //Map container to a random unused port.
+              },
+            ],
+            "30333": [
+              {
+                HostPort: "0", //Map container to a random unused port.
+              },
+            ],
+          },
         },
       },
-    });
-    **/
-    // console.info(JSON.stringify(as));
+      function (err, data, container) {
+        if (err) {
+          return console.error(err);
+        }
+        console.info(data.StatusCode);
+      }
+    );
+    await sleep(5000);
+    const containers = (await docker.listContainers()).filter((x) =>
+      x.Image.includes(dockerImageName)
+    );
+    for (let index = 0; index < containers.length; index++) {
+      const containerInfo = containers[index];
+      await docker.getContainer(containerInfo.Id).remove({ force: true });
+    }
+    await sleep(5000);
   });
   afterEach(async () => {
-    child.exit();
+    const containers = (await docker.listContainers()).filter((x) =>
+      x.Image.includes(dockerImageName)
+    );
+    for (let index = 0; index < containers.length; index++) {
+      const containerInfo = containers[index];
+      await docker.getContainer(containerInfo.Id).remove({ force: true });
+    }
+    await sleep(5000);
   });
 });
