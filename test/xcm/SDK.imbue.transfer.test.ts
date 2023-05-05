4156e3190019d715b2ea49bc2c8cf3d4316d0709
GonCer <gonza.cerro@gmail.com>
2023-05-05 11:48:20 +0200

    fix imbue test and add temp sdk test

---
+++ test/xcm/SDK.imbue.transfer.test.ts
@@ -0,0 +1,137 @@
+import { connectParachains } from "@acala-network/chopsticks";
+import { KeyringPair } from "@polkadot/keyring/types";
+import { BN_THOUSAND } from "@polkadot/util";
+import { AssetId } from "../../utils/ChainSpecs";
+import { ApiContext } from "../../utils/Framework/XcmHelper";
+import XcmNetworks from "../../utils/Framework/XcmNetworks";
+import { devTestingPairs } from "../../utils/setup";
+import { expectJson, matchSystemEvents } from "../../utils/validators";
+import { BN, Mangata } from "@mangata-finance/sdk";
+import { BN_TEN } from "@mangata-finance/sdk";
+import { sleep } from "../../utils/utils";
+
+/**
+ * @group sdkxcm
+ * @group proxied
+ */
+describe("XCM tests for Mangata <-> imbue", () => {
+  let imbue: ApiContext;
+  let mangata: ApiContext;
+  let alice: KeyringPair;
+  const imbueTokenId = 12;
+  beforeAll(async () => {
+    const imbueRocco = await XcmNetworks.imbue({
+      endpoint: "wss://rococo.imbue.network",
+    });
+    imbue = imbueRocco;
+    mangata = await XcmNetworks.mangata({
+      endpoint: "wss://collator-01-ws-rococo.mangata.online",
+    });
+    await connectParachains([imbue.chain, mangata.chain]);
+    alice = devTestingPairs().alice;
+  });
+
+  afterAll(async () => {
+    await imbue.teardown();
+    await mangata.teardown();
+  });
+
+  beforeEach(async () => {
+    await mangata.dev.setStorage({
+      Tokens: {
+        Accounts: [
+          [[alice.address, { token: imbueTokenId }], { free: 1000e12 }],
+          [
+            [alice.address, { token: 0 }],
+            { free: AssetId.Mgx.unit.mul(BN_THOUSAND).toString() },
+          ],
+        ],
+      },
+      Sudo: {
+        Key: alice.address,
+      },
+    });
+    await imbue.dev.setStorage({
+      System: {
+        Account: [[[alice.address], { data: { free: 10e12 } }]],
+      },
+    });
+  });
+
+  it("SDK ROC - mangata transfer assets to imbue", async () => {
+    expectJson(
+      await mangata.api.query.tokens.accounts(alice.address, imbueTokenId)
+    ).toMatchSnapshot("Before");
+
+    expect(await imbue.api.query.system.account(alice.address)).toMatchSnapshot(
+      "Before"
+    );
+    const mgaSdk = Mangata.getInstance([mangata.uri]);
+    const p = mgaSdk.sendTokenFromMangataToParachain(
+      "IMBU",
+      "800000000",
+      2121,
+      alice,
+      alice.address,
+      BN_TEN.mul(BN_TEN.pow(new BN(12)))
+    );
+    await Promise.race([
+      p,
+      new Promise(async () => {
+        await sleep(5000);
+        await mangata.chain.newBlock();
+      }),
+    ]);
+
+    await imbue.chain.newBlock();
+    await sleep(100000);
+    expectJson(
+      await mangata.api.query.tokens.accounts(alice.address, imbueTokenId)
+    ).toMatchSnapshot();
+
+    expect(
+      await imbue.api.query.system.account(alice.address)
+    ).toMatchSnapshot();
+
+    await matchSystemEvents(mangata, "xcmpQueue", "Success");
+    await sleep(1000000);
+  });
+
+  it("SDK ROC - imbue transfer assets to mangata", async () => {
+    expectJson(
+      await mangata.api.query.tokens.accounts(alice.address, imbueTokenId)
+    ).toMatchSnapshot("Before");
+
+    expect(await imbue.api.query.system.account(alice.address)).toMatchSnapshot(
+      "Before"
+    );
+    const mgaSdk = Mangata.getInstance([mangata.uri]);
+    const p = mgaSdk.sendTokenFromParachainToMangata(
+      imbue.uri,
+      "IMBU",
+      "800000000",
+      alice,
+      alice.address,
+      BN_TEN.mul(BN_TEN.pow(new BN(12)))
+    );
+    await Promise.race([
+      p,
+      new Promise(async () => {
+        await sleep(5000);
+        await mangata.chain.newBlock();
+        await imbue.chain.newBlock();
+        await imbue.chain.newBlock();
+      }),
+    ]);
+    expect(
+      await imbue.api.query.system.account(alice.address)
+    ).toMatchSnapshot();
+
+    await mangata.chain.newBlock();
+
+    expectJson(
+      await mangata.api.query.tokens.accounts(alice.address, imbueTokenId)
+    ).toMatchSnapshot();
+    await matchSystemEvents(mangata, "xcmpQueue", "Success");
+  });
+});
