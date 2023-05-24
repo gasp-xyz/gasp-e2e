abed728b28bfe6c84ee60d6da5765c7de9c44e4b
AlexChetverov <alexchetverov@gmail.com>
2023-05-24 13:59:54 +0200

    A test for the sum of the weights was added

--- test/parallel/xyk-pallet.API.pool.weightedPools.test.ts
+++ test/parallel/xyk-pallet.API.pool.weightedPools.test.ts
@@ -6,14 +6,18 @@ import { Keyring } from "@polkadot/api";
 import { getApi, initApi } from "../../utils/api";
 import { Assets } from "../../utils/Assets";
 import { MGA_ASSET_ID } from "../../utils/Constants";
-import { BN, BN_ZERO, signTx } from "@mangata-finance/sdk";
+import { BN, BN_HUNDRED, BN_ZERO, signTx } from "@mangata-finance/sdk";
 import { setupApi, setupUsers } from "../../utils/setup";
 import { Sudo } from "../../utils/sudo";
-import { getLiquidityAssetId, promotePool } from "../../utils/tx";
+import {
+  getLiquidityAssetId,
+  getRewardsInfo,
+  promotePool,
+} from "../../utils/tx";
 import { User } from "../../utils/User";
 import { getEnvironmentRequiredVars, stringToBN } from "../../utils/utils";
 import { Xyk } from "../../utils/xyk";
-import { ExtrinsicResult } from "../../utils/eventListeners";
+import { ExtrinsicResult, waitForRewards } from "../../utils/eventListeners";
 import { getEventResultFromMangataTx } from "../../utils/txHandler";
 
 jest.spyOn(console, "log").mockImplementation(jest.fn());
@@ -108,6 +112,73 @@ test("Validate that weight can be modified by using updatePoolPromotion AND only
   const poolWeightAfter = await getPoolWeight(liqId);
 
   expect(poolWeightAfter).bnEqual(poolWeightBefore.div(new BN(2)));
+});
+
+test("Testing that the sum of the weights can be greater than 100", async () => {
+  const [token2] = await Assets.setupUserWithCurrencies(
+    sudo,
+    [defaultCurrencyValue, defaultCurrencyValue],
+    sudo
+  );
+
+  await Sudo.batchAsSudoFinalized(
+    Assets.mintToken(token2, testUser1, Assets.DEFAULT_AMOUNT),
+    Assets.mintNative(testUser1),
+    Sudo.sudoAs(
+      testUser1,
+      Xyk.createPool(
+        MGA_ASSET_ID,
+        Assets.DEFAULT_AMOUNT.divn(2),
+        token2,
+        Assets.DEFAULT_AMOUNT.divn(2)
+      )
+    )
+  );
+
+  const liqId2 = await getLiquidityAssetId(MGA_ASSET_ID, token2);
+
+  await Sudo.batchAsSudoFinalized(
+    Assets.promotePool(liqId2.toNumber(), 100),
+    Sudo.sudoAs(
+      testUser1,
+      Xyk.activateLiquidity(liqId, Assets.DEFAULT_AMOUNT.divn(2))
+    ),
+    Sudo.sudoAs(
+      testUser1,
+      Xyk.activateLiquidity(liqId2, Assets.DEFAULT_AMOUNT.divn(2))
+    )
+  );
+
+  //await waitForRewards(testUser1, liqId2);
+
+  const poolWeightLiq1 = await getPoolWeight(liqId);
+
+  const poolWeightLiq2 = await getPoolWeight(liqId2);
+
+  const sumPoolsWeights = poolWeightLiq1.add(poolWeightLiq2);
+  const ratioPoolsWeights = poolWeightLiq2.div(poolWeightLiq1);
+
+  await Sudo.batchAsSudoFinalized(
+    Sudo.sudoAs(testUser1, Xyk.claimRewardsAll(liqId)),
+    Sudo.sudoAs(testUser1, Xyk.claimRewardsAll(liqId2))
+  );
+
+  await waitForRewards(testUser1, liqId2);
+
+  const rewardsLiqId1 = await getRewardsInfo(
+    testUser1.keyRingPair.address,
+    liqId
+  );
+
+  const rewardsLiqId2 = await getRewardsInfo(
+    testUser1.keyRingPair.address,
+    liqId2
+  );
+
+  expect(sumPoolsWeights).bnGt(BN_HUNDRED);
+  expect(rewardsLiqId1.rewardsAlreadyClaimed.mul(ratioPoolsWeights)).bnLte(
+    rewardsLiqId2.rewardsAlreadyClaimed
+  );
 });
 
 async function getPoolWeight(tokenId: BN) {
