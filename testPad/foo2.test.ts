10d331a1c60423a9b011cca969f996aee9da48ce
GonCer <gonza.cerro@gmail.com>
2023-06-14 15:01:47 +0200

    small helper to get all avl. methods in sdk

--- testPad/foo2.test.ts
+++ testPad/foo2.test.ts
@@ -3,13 +3,10 @@ import { jest } from "@jest/globals";
 import { testLog } from "../utils/Logger";
 import dotenv from "dotenv";
 import { setupApi, setupUsers } from "../utils/setup";
-import { Assets } from "../utils/Assets";
-import { BN } from "@polkadot/util";
 import { Keyring } from "@polkadot/api";
 import { initApi } from "../utils/api";
-import { getEnvironmentRequiredVars } from "../utils/utils";
 import { User } from "../utils/User";
-import { getBalanceOfAsset } from "../utils/tx";
+import { Mangata } from "@mangata-finance/sdk";
 
 dotenv.config();
 
@@ -18,26 +15,30 @@ jest.spyOn(console, "log").mockImplementation(jest.fn());
 jest.setTimeout(1500000);
 process.env.NODE_ENV = "test";
 
-test("asdasdasd", async () => {
-  const { sudo: sudoUserName } = getEnvironmentRequiredVars();
-  const defaultCurrecyValue = new BN(250000);
+test("find all available functions in sdk.", async () => {
   await initApi();
-  const keyring = new Keyring({ type: "sr25519" });
-
-  // setup users
-  const testUser1 = new User(keyring);
-  const sudo = new User(keyring, sudoUserName);
-  testLog.getLog().info(testUser1.keyRingPair.address);
   await setupApi();
   await setupUsers();
-  const s = await getBalanceOfAsset(new BN(0), testUser1);
-  const [firstCurrency, secondCurrency] = await Assets.setupUserWithCurrencies(
-    testUser1,
-    [defaultCurrecyValue, defaultCurrecyValue.add(new BN(1))],
-    sudo
-  );
-
-  testLog.getLog().info(JSON.stringify(firstCurrency));
-  testLog.getLog().info(JSON.stringify(secondCurrency));
+  const keyring = new Keyring({ type: "sr25519" });
+  // setup users
+  const testUser1 = new User(keyring);
+  testLog.getLog().info(testUser1.keyRingPair.address);
+  const s = Mangata.instance(["ws://127.0.0.1:9946"]);
+  findMethods(s, 0);
   testLog.getLog().info(JSON.stringify(s));
 });
+function findMethods(o: any, num: number) {
+  for (const a in o) {
+    //@ts-ignore
+    if (typeof o[a] === "function" && o.hasOwnProperty(a)) {
+      let str = JSON.stringify(a);
+      if (num > 0) {
+        str = " -> " + str;
+      }
+      testLog.getLog().info(str);
+    } else if (typeof o[a] === "object" && o.hasOwnProperty(a)) {
+      testLog.getLog().info(JSON.stringify(a) + "::");
+      findMethods(o[a], num++);
+    }
+  }
+}
