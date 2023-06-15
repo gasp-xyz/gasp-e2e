bfd8e380d380016d90539a705d73c5a4d1a0fb41
AlexChetverov <alexchetverov@gmail.com>
2023-06-15 14:36:33 +0200

    test Staking.executeBondRequest was added

--- test/parallel/xyk-pallet.API.parachainStacking.joinCandidate.test.ts
+++ test/parallel/xyk-pallet.API.parachainStacking.joinCandidate.test.ts
@@ -7,7 +7,7 @@
 import { getApi, initApi } from "../../utils/api";
 import { BN } from "@polkadot/util";
 import { Keyring } from "@polkadot/api";
-import { User } from "../../utils/User";
+import { AssetWallet, User } from "../../utils/User";
 import { Assets } from "../../utils/Assets";
 import { getEnvironmentRequiredVars } from "../../utils/utils";
 import { MGA_ASSET_ID } from "../../utils/Constants";
@@ -16,12 +16,15 @@ import { getLiquidityAssetId, joinCandidate } from "../../utils/tx";
 import { ExtrinsicResult } from "../../utils/eventListeners";
 import { Staking } from "../../utils/Staking";
 import { Xyk } from "../../utils/xyk";
+import { waitNewStakingRound } from "../../utils/utils";
+import { BN_ZERO, signTx } from "@mangata-finance/sdk";
 
 jest.spyOn(console, "log").mockImplementation(jest.fn());
 jest.spyOn(console, "error").mockImplementation(jest.fn());
 jest.setTimeout(1500000);
 process.env.NODE_ENV = "test";
 
+let testUser: User;
 let testUser1: User;
 
 let sudo: User;
@@ -35,7 +38,7 @@ let minStk: BN;
 
 const { sudo: sudoUserName } = getEnvironmentRequiredVars();
 
-beforeEach(async () => {
+beforeAll(async () => {
   try {
     getApi();
   } catch (e) {
@@ -45,16 +48,15 @@ beforeEach(async () => {
   keyring = new Keyring({ type: "sr25519" });
 
   // setup users
-  testUser1 = new User(keyring);
+  testUser = new User(keyring);
   sudo = new User(keyring, sudoUserName);
 
   minStk = new BN(
     (await getApi()).consts.parachainStaking.minCandidateStk.toString()
   );
 
-  //add two currencies and balance to testUser:
   [tokenId] = await Assets.setupUserWithCurrencies(
-    testUser1,
+    testUser,
     [minStk.muln(1000), minStk.muln(1000)],
     sudo
   );
@@ -62,21 +64,37 @@ beforeEach(async () => {
   await Sudo.batchAsSudoFinalized(
     Assets.FinalizeTge(),
     Assets.initIssuance(),
-    Assets.mintNative(testUser1, minStk.muln(1000)),
+    Assets.mintNative(testUser, minStk.muln(1000)),
     Sudo.sudoAs(
-      testUser1,
+      testUser,
       Xyk.createPool(MGA_ASSET_ID, minStk.muln(3), tokenId, minStk.muln(3))
     )
   );
-});
 
-test("A User with free tokens can join as collator", async () => {
   liqToken = await getLiquidityAssetId(MGA_ASSET_ID, tokenId);
 
   await Sudo.asSudoFinalized(
     Sudo.sudo(Staking.addStakingLiquidityToken(liqToken))
   );
+});
 
+beforeEach(async () => {
+  testUser1 = new User(keyring);
+
+  await Sudo.batchAsSudoFinalized(
+    Assets.mintNative(testUser1, minStk.muln(1000)),
+    Assets.mintToken(tokenId, testUser1, minStk.muln(1000))
+  );
+
+  await Sudo.batchAsSudoFinalized(
+    Sudo.sudoAs(
+      testUser1,
+      Xyk.mintLiquidity(MGA_ASSET_ID, tokenId, minStk.muln(5), minStk.muln(6))
+    )
+  );
+});
+
+test("A User with free tokens can join as collator", async () => {
   const result = await joinCandidate(
     testUser1.keyRingPair,
     liqToken,
@@ -87,4 +105,50 @@ test("A User with free tokens can join as collator", async () => {
     testUser1.keyRingPair.address
   );
   expect(isUserInCandidate).toBeTruthy();
+});
+
+test("A user can schedule bond more", async () => {
+  const api = getApi();
+
+  testUser1.addAsset(liqToken);
+
+  await testUser1.refreshAmounts(AssetWallet.BEFORE);
+
+  const userCurrencyBeforeJoining =
+    testUser1.getAsset(liqToken)?.amountBefore.reserved!;
+
+  await joinCandidate(testUser1.keyRingPair, liqToken, minStk.muln(2)).then(
+    (result) => {
+      expect(result.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
+    }
+  );
+
+  await testUser1.refreshAmounts(AssetWallet.BEFORE);
+
+  const userCurrencyBeforeExecuting =
+    testUser1.getAsset(liqToken)?.amountBefore.reserved!;
+
+  await signTx(
+    api,
+    Staking.scheduleCandidateBondMore(minStk),
+    testUser1.keyRingPair
+  );
+
+  await waitNewStakingRound();
+  await waitNewStakingRound();
+
+  await signTx(
+    api,
+    Staking.executeBondRequest(testUser1),
+    testUser1.keyRingPair
+  );
+
+  await testUser1.refreshAmounts(AssetWallet.AFTER);
+
+  const userCurrencyAfterExecuting =
+    testUser1.getAsset(liqToken)?.amountAfter.reserved!;
+
+  expect(userCurrencyBeforeJoining).bnEqual(BN_ZERO);
+  expect(userCurrencyBeforeExecuting).bnEqual(minStk.muln(2));
+  expect(userCurrencyAfterExecuting).bnEqual(minStk.muln(3));
 });
