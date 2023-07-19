42d8f06450e5b0b070e9244db7ddf373e71a70ca
MariuSZ <mariusz.szpyt@mangata.finance>
2023-07-19 11:36:52 +0200

    Update microapps-ui-main.deposit.test.ts

--- test/microapps-ui-xcm/microapps-ui-main.deposit.test.ts
+++ test/microapps-ui-xcm/microapps-ui-main.deposit.test.ts
@@ -134,6 +134,7 @@ describe("Microapps UI deposit modal tests - no action", () => {
     await depositModal.waitForTokenListElementsVisible(KSM_ASSET_NAME);
     await depositModal.selectToken(KSM_ASSET_NAME);
     await depositModal.enterValue("1");
+    await kusama.chain.newBlock();
     await depositModal.waitForContinueState(true, 20000);
 
     const isContinueButtonEnabled =
