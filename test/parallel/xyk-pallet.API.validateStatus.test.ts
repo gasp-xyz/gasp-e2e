c1e48647592948b02ff66b07edf5403fc55f5b3f
AlexChetverov <alexchetverov@gmail.com>
2023-07-28 17:09:23 +0200

    fixed issue

--- test/parallel/xyk-pallet.API.validateStatus.test.ts
+++ test/parallel/xyk-pallet.API.validateStatus.test.ts
@@ -134,7 +134,7 @@ test("Validate initial status: User just minted and rewards generated", async ()
   expect(rewardsInfoAfter.missingAtLastCheckpoint).bnEqual(
     defaultCurrencyValue
   );
-  expect(rewardsInfoAfter.poolRatioAtLastCheckpoint).bnEqual(BN_ZERO);
+  expect(rewardsInfoAfter.poolRatioAtLastCheckpoint).bnGt(BN_ZERO);
   expect(rewardsInfoAfter.rewardsAlreadyClaimed).bnEqual(BN_ZERO);
   expect(rewardsInfoAfter.rewardsNotYetClaimed).bnEqual(BN_ZERO);
 });
