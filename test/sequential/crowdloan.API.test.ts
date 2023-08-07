9d0ad7ca6d90f6e57a4e14f52ca5d549ac7d3cc3
AlexChetverov <alexchetverov@gmail.com>
2023-08-07 17:35:50 +0200

    added first test

---
+++ test/sequential/crowdloan.API.test.ts
@@ -0,0 +1,94 @@
+/*
+ *
+ *
+ */
+import { jest } from "@jest/globals";
+import { ApiPromise, Keyring } from "@polkadot/api";
+import { getApi, initApi } from "../../utils/api";
+import { Assets } from "../../utils/Assets";
+import { BN } from "@polkadot/util";
+import { setupApi, setupUsers } from "../../utils/setup";
+import { Sudo } from "../../utils/sudo";
+import { User } from "../../utils/User";
+import { getEnvironmentRequiredVars } from "../../utils/utils";
+import { Xyk } from "../../utils/xyk";
+import { MGA_ASSET_ID } from "../../utils/Constants";
+import { signTx } from "@mangata-finance/sdk";
+import { getEventResultFromMangataTx } from "../../utils/txHandler";
+import {
+  ExtrinsicResult,
+  waitSudoOperationSuccess,
+} from "../../utils/eventListeners";
+
+jest.spyOn(console, "log").mockImplementation(jest.fn());
+jest.setTimeout(2500000);
+process.env.NODE_ENV = "test";
+
+const { sudo: sudoUserName } = getEnvironmentRequiredVars();
+let testUser1: User;
+let api: ApiPromise;
+let sudo: User;
+let keyring: Keyring;
+let firstToken: BN;
+let secondToken: BN;
+const millionNative = new BN("1000000000000000000000000");
+const nativeCurrencyId = MGA_ASSET_ID;
+const defaultCurrencyValue = new BN(10000000);
+const defaultPoolVolumeValue = new BN(1000000);
+
+beforeAll(async () => {
+  try {
+    getApi();
+  } catch (e) {
+    await initApi();
+  }
+  keyring = new Keyring({ type: "sr25519" });
+
+  api = getApi();
+  sudo = new User(keyring, sudoUserName);
+});
+
+beforeEach(async () => {
+  await setupApi();
+
+  [testUser1] = setupUsers();
+
+  [firstToken, secondToken] = await Assets.setupUserWithCurrencies(
+    testUser1,
+    [defaultCurrencyValue, defaultCurrencyValue],
+    sudo
+  );
+  testUser1.addAsset(nativeCurrencyId);
+  await Sudo.batchAsSudoFinalized(
+    Assets.mintNative(sudo),
+    Assets.mintNative(testUser1),
+    Sudo.sudoAs(
+      testUser1,
+      Xyk.createPool(
+        firstToken,
+        defaultPoolVolumeValue,
+        secondToken,
+        defaultPoolVolumeValue
+      )
+    )
+  );
+});
+
+test("Only sudo can crowdloan.setCrowdloanAllocation(crowdloanAllocationAmount)", async () => {
+  const userSetCrowdloanAllocation = await signTx(
+    api,
+    api.tx.crowdloan.setCrowdloanAllocation(millionNative),
+    testUser1.keyRingPair
+  );
+
+  const eventResponse = getEventResultFromMangataTx(userSetCrowdloanAllocation);
+
+  expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicFailed);
+  expect(eventResponse.data).toEqual("UnknownError");
+
+  const sudoSetCrowdloanAllocation = await Sudo.batchAsSudoFinalized(
+    Sudo.sudoAs(sudo, api.tx.crowdloan.setCrowdloanAllocation(millionNative))
+  );
+
+  await waitSudoOperationSuccess(sudoSetCrowdloanAllocation);
+});
