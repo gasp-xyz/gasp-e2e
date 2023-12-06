bfb62a2af769bfe243d2bdb2a0ebfa0b294b210b
MariuSZ <112616777+MariuszSzpyt@users.noreply.github.com>
2023-12-06 15:40:43 +0100

    Feature/microapps prod checks (#356)

---
+++ test/microapps-prod/microapps-prod-main.wallet.test.ts
@@ -0,0 +1,110 @@
+/*
+ *
+ * @group microappsProdWallet
+ */
+import { jest } from "@jest/globals";
+import { Keyring } from "@polkadot/api";
+import { WebDriver } from "selenium-webdriver";
+import { getApi, initApi } from "../../utils/api";
+import { DriverBuilder } from "../../utils/frontend/utils/Driver";
+import {
+  addExtraLogs,
+  importPolkadotExtension,
+} from "../../utils/frontend/utils/Helper";
+import { AssetWallet, User } from "../../utils/User";
+import { getEnvironmentRequiredVars } from "../../utils/utils";
+import { KSM_ASSET_ID, MGA_ASSET_ID } from "../../utils/Constants";
+import { Node } from "../../utils/Framework/Node/Node";
+import "dotenv/config";
+import {
+  connectWallet,
+  setupPage,
+  setupPageWithState,
+} from "../../utils/frontend/microapps-utils/Handlers";
+import { WalletWrapper } from "../../utils/frontend/microapps-pages/WalletWrapper";
+
+jest.spyOn(console, "log").mockImplementation(jest.fn());
+
+jest.setTimeout(1500000);
+let driver: WebDriver;
+let testUser1: User;
+
+const acc_name = "acc_automation";
+const KSM_ASSET_NAME = "KSM";
+const MGX_ASSET_NAME = "MGX";
+const TUR_ASSET_NAME = "TUR";
+
+describe("Microapps Prod UI wallet tests", () => {
+  beforeAll(async () => {
+    try {
+      getApi();
+    } catch (e) {
+      await initApi();
+    }
+
+    driver = await DriverBuilder.getInstance();
+    await importPolkadotExtension(driver);
+
+    const keyring = new Keyring({ type: "sr25519" });
+    const node = new Node(getEnvironmentRequiredVars().chainUri);
+    await node.connect();
+
+    testUser1 = new User(keyring);
+    testUser1.addFromMnemonic(
+      keyring,
+      getEnvironmentRequiredVars().mnemonicPolkadot,
+    );
+
+    testUser1.addAsset(KSM_ASSET_ID);
+    testUser1.addAsset(MGA_ASSET_ID);
+    await testUser1.refreshAmounts(AssetWallet.BEFORE);
+
+    await setupPage(driver);
+    await connectWallet(driver, "Polkadot", acc_name);
+  });
+
+  test("User can see his tokens and amounts", async () => {
+    await setupPageWithState(driver, acc_name);
+
+    const walletWrapper = new WalletWrapper(driver);
+    await walletWrapper.openWalletConnectionInfo();
+    const isKSM = await walletWrapper.isMyTokensRowDisplayed(KSM_ASSET_NAME);
+    expect(isKSM).toBeFalsy();
+    const isMGX = await walletWrapper.isMyTokensRowDisplayed(MGX_ASSET_NAME);
+    expect(isMGX).toBeTruthy();
+    const isTUR = await walletWrapper.isMyTokensRowDisplayed(TUR_ASSET_NAME);
+    expect(isTUR).toBeTruthy();
+
+    const mgxAmount = await walletWrapper.getMyTokensRowAmount(MGX_ASSET_NAME);
+    expect(parseFloat(mgxAmount)).toBeGreaterThan(0);
+
+    const turAmount = await walletWrapper.getMyTokensRowAmount(TUR_ASSET_NAME);
+    expect(parseFloat(turAmount)).toBeGreaterThan(0);
+  });
+
+  test("User can see his pool positions", async () => {
+    await setupPageWithState(driver, acc_name);
+
+    const walletWrapper = new WalletWrapper(driver);
+    await walletWrapper.openWalletConnectionInfo();
+    await walletWrapper.pickMyPositions();
+    const POOL_NAME = "TUR - MGX";
+    const isTurMgx = await walletWrapper.isMyPositionsRowDisplayed(POOL_NAME);
+    expect(isTurMgx).toBeTruthy();
+  });
+
+  afterEach(async () => {
+    const session = await driver.getSession();
+    await addExtraLogs(
+      driver,
+      expect.getState().currentTestName + " - " + session.getId(),
+    );
+  });
+
+  afterAll(async () => {
+    const api = getApi();
+    await api.disconnect();
+    await driver.quit();
+    DriverBuilder.destroy();
+  });
+});
