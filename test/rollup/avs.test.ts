9918f0521865370d5a138d8f6cfb7bc7824f90e0
GonCer <gonza.cerro@gmail.com>
2024-03-18 16:04:39 +0100

    check task produced and stored on avs
    Signed-off-by: GonCer <gonza.cerro@gmail.com>

---
+++ test/rollup/avs.test.ts
@@ -0,0 +1,99 @@
+/*
+ *
+ * @group rollup
+ */
+import "jest-extended";
+import { PublicClient } from "viem";
+import { publicClient } from "../../utils/rollup/ethUtils";
+import { jest } from "@jest/globals";
+import { testLog } from "../../utils/Logger";
+
+jest.setTimeout(600000);
+const taskManagerAddress = "0x9E545E3C0baAB3E08CdfD552C960A1050f373042";
+
+function waitForTaskGenerated(publicClient: PublicClient) {
+  return new Promise((resolve, _) => {
+    publicClient.watchEvent({
+      address: taskManagerAddress,
+      event: {
+        type: "event",
+        name: "NewTaskCreated",
+        inputs: [
+          {
+            name: "taskIndex",
+            type: "uint32",
+            indexed: true,
+            internalType: "uint32",
+          },
+          {
+            name: "task",
+            type: "(uint256,uint32,bytes,uint32)",
+            indexed: false,
+            internalType: "(uint256,uint32,bytes,uint32)",
+          },
+        ],
+        anonymous: false,
+      },
+      onLogs: async (logs) => {
+        for (const log of logs) {
+          // @ts-ignore
+          const taskIndex: number = log.args.taskIndex;
+          testLog.getLog().info(JSON.stringify(log));
+          resolve(taskIndex);
+        }
+      },
+    });
+  });
+}
+
+function waitForTaskResponded(publicClient: PublicClient) {
+  return new Promise((resolve, _) => {
+    publicClient.watchEvent({
+      address: taskManagerAddress,
+      event: {
+        type: "event",
+        name: "TaskResponded",
+        inputs: [
+          {
+            name: "taskResponse",
+            type: "(uint32,bytes32,bytes32)",
+            indexed: false,
+            internalType: "(uint32,bytes32,bytes32)",
+          },
+          {
+            name: "taskResponseMetadata",
+            type: "(uint32,bytes32,uint96[],uint96[])",
+            indexed: false,
+            internalType: "(uint32,bytes32,uint96[],uint96[])",
+          },
+        ],
+        anonymous: false,
+      },
+      onLogs: async (logs) => {
+        for (const log of logs) {
+          // @ts-ignore
+          testLog.getLog().info(JSON.stringify(log));
+          resolve(log);
+        }
+      },
+    });
+  });
+}
+
+describe("Rollup", () => {
+  describe("AVS", () => {
+    beforeEach(async () => {});
+
+    test("Aggregator - Tasks are generated", async () => {
+      await waitForTaskGenerated(publicClient);
+    });
+    test("Aggregator & Finalizer - Responses are written in aws", async () => {
+      await waitForTaskResponded(publicClient);
+    });
+  });
+});
+
+// @ts-ignore
+BigInt.prototype["toJSON"] = function () {
+  return this.toString();
+};
