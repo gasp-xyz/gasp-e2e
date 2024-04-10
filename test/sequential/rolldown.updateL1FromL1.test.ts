765c5ce522bf682650e5b7b274a056f301115e5c
GonCer <gonza.cerro@gmail.com>
2024-04-10 10:32:38 +0200

    small improvements
    Signed-off-by: GonCer <gonza.cerro@gmail.com>

--- test/sequential/rolldown.updateL1FromL1.test.ts
+++ test/sequential/rolldown.updateL1FromL1.test.ts
@@ -11,24 +11,37 @@ import { initApi } from "../../utils/api";
 import { setupUsers } from "../../utils/setup";
 import { expectExtrinsicSucceed } from "../../utils/utils";
 
-describe('updateL1FromL1', () => {
-  let sequencer : EthUser;
+describe("updateL1FromL1", () => {
+  let sequencer: EthUser;
   beforeEach(async () => {
     await initApi();
     setupUsers();
     sequencer = await SequencerStaking.getSequencerUser();
   });
-  it("Add twice the same requestId", async () => {
+  it("Add twice the same deposit with requestId", async () => {
     const txIndex = await Rolldown.l2OriginRequestId();
-    const res = await
-      Rolldown.deposit(sequencer, txIndex, sequencer.ethAddress, BN_THOUSAND);
+    const res = await Rolldown.deposit(
+      sequencer,
+      txIndex,
+      sequencer.ethAddress,
+      BN_THOUSAND,
+    );
     expectExtrinsicSucceed(res);
     const events = await Rolldown.untilL2Processed(res);
-    const res2 = await
-      Rolldown.deposit(sequencer, txIndex, sequencer.ethAddress, BN_THOUSAND);
+    expect(
+      Rolldown.isDepositSucceed(events, sequencer.ethAddress, BN_THOUSAND),
+    ).toBe(true);
+    const res2 = await Rolldown.deposit(
+      sequencer,
+      txIndex,
+      sequencer.ethAddress,
+      BN_THOUSAND,
+    );
     expectExtrinsicSucceed(res2);
     const events2 = await Rolldown.untilL2Processed(res2);
-
+    expect(
+      Rolldown.isDepositSucceed(events2, sequencer.ethAddress, BN_THOUSAND),
+    ).toBe(false);
     expect(events.length).toBeGreaterThan(2);
     expect(events2.length).toBe(2);
   });
--- utils/rollDown/Rolldown.ts
+++ utils/rollDown/Rolldown.ts
@@ -9,7 +9,6 @@ import { getEventsAt } from "../eventListeners";
 import { ApiPromise } from "@polkadot/api";
 
 export class Rolldown {
-
   static async l2OriginRequestId(l1 = "Ethereum") {
     setupUsers();
     const api = getApi();
@@ -22,48 +21,81 @@ export class Rolldown {
     const requestId = await api.query.rolldown.lastProcessedRequestOnL2(l1);
     return requestId as any as number;
   }
-
+  static isDepositSucceed(
+    events: MangataGenericEvent[],
+    userAddress: string,
+    amount: BN,
+  ) {
+    return events.some(
+      (x) =>
+        x.phase.toString() === "Initialization" &&
+        x.event.method === "Deposited" &&
+        x.event.section === "tokens" &&
+        JSON.stringify(x.event.data).includes(userAddress.toLowerCase()) &&
+        Object.hasOwn(x.event.data, "amount") &&
+        stringToBN(
+          // @ts-ignore : it's secure to access the amount property
+          x.event.data["amount"].toString(),
+        ).eq(amount),
+    );
+  }
   static async untilL2Processed(txResult: MangataGenericEvent[]) {
-    const until = getEventResultFromMangataTx(txResult, ["rolldown", "L1ReadStored"]);
+    const until = getEventResultFromMangataTx(txResult, [
+      "rolldown",
+      "L1ReadStored",
+    ]);
     const blockNo = stringToBN(until.data[0][1]);
     await waitBlockNumber(blockNo.toString(), 10);
     const events = await getEventsAt(blockNo);
     return events as any[] as MangataGenericEvent[];
   }
-  static async deposit(user : EthUser, requestIdx: number, ethAddress: string, amount: BN) {
-    const tx =
-      new L2Update(getApi())
-        .withDeposit(requestIdx, ethAddress, ethAddress, amount.toNumber())
-        .build();
+  static async deposit(
+    user: EthUser,
+    requestIdx: number,
+    ethAddress: string,
+    amount: BN,
+  ) {
+    const tx = new L2Update(getApi())
+      .withDeposit(requestIdx, ethAddress, ethAddress, amount.toNumber())
+      .build();
     const api = getApi();
     return await signTx(api, tx, user.keyRingPair);
   }
-
 }
 export class L2Update {
   api: ApiPromise;
   pendingDeposits: any[];
   constructor(api: ApiPromise) {
-    this.api = api
-    this.pendingDeposits = this.api.createType("Vec<PalletRolldownMessagesDeposit>");
+    this.api = api;
+    this.pendingDeposits = this.api.createType(
+      "Vec<PalletRolldownMessagesDeposit>",
+    );
   }
 
-  build(){
+  build() {
     return this.api.tx.rolldown.updateL2FromL1({
-      pendingDeposits: this.api.createType("Vec<PalletRolldownMessagesDeposit>",  this.pendingDeposits),
+      pendingDeposits: this.api.createType(
+        "Vec<PalletRolldownMessagesDeposit>",
+        this.pendingDeposits,
+      ),
     });
   }
-  withDeposit(txIndex: number, ethAddress: string, erc20Address: string, amountValue: number) {
-    const deposit =  this.api.createType("PalletRolldownMessagesDeposit",  {
-        requestId: this.api.createType("PalletRolldownMessagesRequestId", [
-          "L1",
-          txIndex,
-        ]),
-        depositRecipient: ethAddress,
-        tokenAddress: erc20Address,
-        amount: amountValue,
-        blockHash: ethAddress + "000000000000000000000000",
-      });
+  withDeposit(
+    txIndex: number,
+    ethAddress: string,
+    erc20Address: string,
+    amountValue: number,
+  ) {
+    const deposit = this.api.createType("PalletRolldownMessagesDeposit", {
+      requestId: this.api.createType("PalletRolldownMessagesRequestId", [
+        "L1",
+        txIndex,
+      ]),
+      depositRecipient: ethAddress,
+      tokenAddress: erc20Address,
+      amount: amountValue,
+      blockHash: ethAddress + "000000000000000000000000",
+    });
     this.pendingDeposits.push(deposit);
     return this;
   }
