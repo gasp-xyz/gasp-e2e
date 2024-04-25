a58740522f0603a50b3fb997fb0a27b335c4afa3
MariuSZ <mariusz.szpyt@mangata.finance>
2024-04-25 10:56:05 +0200

    refactor mock

--- utils/wsMock.cjs
+++ utils/wsMock.cjs
@@ -11,36 +11,16 @@ wss.on("connection", (ws) => {
 
   ws.on("message", (message) => {
     console.log(`Received message: ${message}`);
-    const headers = {};
-    if (message.headers) {
-      Object.keys(message.headers).forEach((key) => {
-        headers[key] = message.headers[key];
-      });
-    }
 
     try {
       const messageJson = JSON.parse(message);
 
       switch (messageJson.method) {
         case "rpc_methods":
-          const rpcResponseJson = fs.readFileSync(
-            "rpc_methods_response.json",
-            "utf8"
-          );
-          clients.forEach((client) => {
-            client.send(rpcResponseJson);
-          });
+          handleRpcMethods();
           break;
         case "xyk_get_burn_amount":
-          const burnResponseJson = fs.readFileSync(
-            "get_burn_amount_response.json",
-            "utf8"
-          );
-          let burnResponseParsed = JSON.parse(burnResponseJson);
-          burnResponseParsed.id = messageJson.id;
-          clients.forEach((client) => {
-            client.send(JSON.stringify(burnResponseParsed));
-          });
+          handleGetBurnAmount(messageJson);
           break;
         default:
           console.log("Unknown method:", messageJson.method);
@@ -48,7 +28,7 @@ wss.on("connection", (ws) => {
             remoteWs = new WebSocket("ws://localhost:9946");
             remoteWs.on("open", () => {
               // Send the message to the remote server
-              remoteWs.send(message, { headers });
+              remoteWs.send(message);
             });
             remoteWs.on("message", (message) => {
               const resHeaders = {};
@@ -63,15 +43,13 @@ wss.on("connection", (ws) => {
                 const response = message.toString();
                 client.send(response, { headers: resHeaders });
               });
-              console.log(
-                `Received response: ${message} with headers: ${message.headers}`
-              );
+              console.log(`Received response: ${message} with headers: ${message.headers}`);
             });
             remoteWs.on("close", () => {
               console.log("Connection closed");
             });
           } else {
-            remoteWs.send(message, { headers });
+            remoteWs.send(message);
           }
       }
     } catch (error) {
@@ -83,3 +61,22 @@ wss.on("connection", (ws) => {
     console.log("Client disconnected");
   });
 });
+
+function handleRpcMethods() {
+  const rpcResponseJson = fs.readFileSync("rpc_methods_response.json", "utf8");
+  console.log("Mocked response:", rpcResponseJson);
+  clients.forEach((client) => {
+    client.send(rpcResponseJson);
+  });
+}
+
+function handleGetBurnAmount(messageJson) {
+  const burnResponseJson = fs.readFileSync("get_burn_amount_response.json", "utf8");
+  let burnResponseParsed = JSON.parse(burnResponseJson);
+  burnResponseParsed.id = messageJson.id;
+  let response = JSON.stringify(burnResponseParsed);
+  console.log("Mocked response:", response);
+  clients.forEach((client) => {
+    client.send(response);
+  });
+}
