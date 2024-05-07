f36862f9e28cd41ea39ebc29308bc94af2cc239a
MariuSZ <mariusz.szpyt@mangata.finance>
2024-05-07 11:18:57 +0200

    Update rollup-main.metamask.test.ts

--- test/rollup-test/rollup-main.metamask.test.ts
+++ test/rollup-test/rollup-main.metamask.test.ts
@@ -3,7 +3,6 @@
  * @group rollupWallet
  */
 import { jest } from "@jest/globals";
-import { Keyring } from "@polkadot/api";
 import { WebDriver } from "selenium-webdriver";
 import { getApi, initApi } from "../../utils/api";
 import { DriverBuilder } from "../../utils/frontend/utils/Driver";
@@ -11,10 +10,6 @@ import {
   addExtraLogs,
   importMetamaskExtension,
 } from "../../utils/frontend/utils/Helper";
-import { AssetWallet, User } from "../../utils/User";
-import { getEnvironmentRequiredVars } from "../../utils/utils";
-import { KSM_ASSET_ID, MGA_ASSET_ID } from "../../utils/Constants";
-import { Node } from "../../utils/Framework/Node/Node";
 import "dotenv/config";
 import { WalletWrapper } from "../../utils/frontend/microapps-pages/WalletWrapper";
 import {
@@ -28,7 +23,6 @@ jest.spyOn(console, "log").mockImplementation(jest.fn());
 
 jest.setTimeout(1500000);
 let driver: WebDriver;
-let testUser1: User;
 
 let acc_addr = "";
 let acc_addr_short = "";
@@ -44,20 +38,6 @@ describe("Microapps Prod UI wallet tests", () => {
     driver = await DriverBuilder.getInstance();
     acc_addr = await importMetamaskExtension(driver);
     acc_addr_short = acc_addr.slice(-4).toLowerCase();
-
-    const keyring = new Keyring({ type: "ecdsa" });
-    const node = new Node(getEnvironmentRequiredVars().chainUri);
-    await node.connect();
-
-    testUser1 = new User(keyring);
-    testUser1.addFromMnemonic(
-      keyring,
-      getEnvironmentRequiredVars().mnemonicMetaMask,
-    );
-
-    testUser1.addAsset(KSM_ASSET_ID);
-    testUser1.addAsset(MGA_ASSET_ID);
-    await testUser1.refreshAmounts(AssetWallet.BEFORE);
 
     await setupPage(driver);
     await connectWallet(driver, "Metamask", acc_addr_short);
@@ -86,7 +66,7 @@ describe("Microapps Prod UI wallet tests", () => {
     const session = await driver.getSession();
     await addExtraLogs(
       driver,
-      expect.getState().currentTestName + " - " + session.getId(),
+      expect.getState().currentTestName + " - " + session.getId()
     );
   });
 
