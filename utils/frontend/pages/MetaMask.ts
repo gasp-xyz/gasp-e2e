ef3c13a97f5b50786290f95a13d5928ec01d9aec
MariuSZ <mariusz.szpyt@mangata.finance>
2024-10-09 09:26:04 +0200

    new metamask

--- utils/frontend/pages/MetaMask.ts
+++ utils/frontend/pages/MetaMask.ts
@@ -1,5 +1,5 @@
 import { WebDriver } from "selenium-webdriver";
-import { getEnvironmentRequiredVars } from "../../utils";
+import { getEnvironmentRequiredVars, sleep } from "../../utils";
 import {
   appendText,
   buildClassXpath,
@@ -38,18 +38,18 @@ const BTN_ACCOUNT_DETAILS = "account-list-menu-details";
 const BTN_ACCOUNT_LABEL = "editable-label-button";
 const DIV_ACCOUNT_LABEL_INPUT = "editable-input";
 const BTN_CONNECT_ACCOUNT = "page-container-footer-next";
-const BTN_COPY_ADDRESS = "address-copy-button-text";
 const BTN_ACC_SELECTION = "account-menu-icon";
 const BTN_IMPORT_ACCOUNT = "multichain-account-menu-popover-action-button";
 const BTN_IMPORT_ACCOUNT_CONFIRM = "import-account-confirm-button";
 const BTN_FOOTER_NEXT = "page-container-footer-next";
 const BTN_GENERIC_CONFIRMATION = "confirmation-submit-button";
-const BTN_CONFIRM_TRANSACTION = "confirm-footer-confirm-button";
+const BTN_CONFIRM_TRANSACTION = "confirm-footer-button";
 const BTN_REJECT_TRANSACTION = "page-container-footer-cancel";
+let originalWindowHandle: string;
 
 export class MetaMask {
   WEB_UI_ACCESS_URL =
-    "chrome-extension://ibkogccjfnojapecnljinaondndgildg/home.html";
+    "chrome-extension://mgmaidbjhpkihbbdppiimfibmgehjkgb/home.html";
 
   private static instance: MetaMask;
   private constructor(driver: WebDriver) {
@@ -81,6 +81,24 @@ export class MetaMask {
     await this.driver.get(this.WEB_UI_ACCESS_URL);
   }
 
+  static async openMetaMaskInNewTab(driver: WebDriver) {
+    originalWindowHandle = await driver.getWindowHandle();
+    await driver.executeScript("window.open()");
+    const handles = await driver.getAllWindowHandles();
+    // Switch to the newly opened tab (last in the list of handles)
+    await driver.switchTo().window(handles[handles.length - 1]);
+    await MetaMask.getInstance(driver).go();
+    await sleep(3000);
+  }
+
+  static async switchBackToOriginalTab(driver: WebDriver) {
+    if (originalWindowHandle) {
+        await driver.switchTo().window(originalWindowHandle);
+    } else {
+        console.warn('Original window handle not found.');
+    }
+}
+
   async setupAccount(mnemonicKeys = this.mnemonicMetaMask): Promise<string> {
     await this.driver.get(
       `${this.WEB_UI_ACCESS_URL}#onboarding/import-with-recovery-phrase`,
@@ -89,11 +107,11 @@ export class MetaMask {
     const XPATH_FIRST_WORD = buildDataTestIdXpath(IMPUT_MNEMONIC_FIELD + 0);
     await waitForElement(this.driver, XPATH_FIRST_WORD);
 
-    try {
-      await this.closeAnyExtraWindow(this.driver);
-    } catch (e) {
-      //no window to close
-    }
+    // try {
+    //   await this.closeAnyExtraWindow(this.driver);
+    // } catch (e) {
+    //   //no window to close
+    // }
 
     await this.fillPassPhrase(mnemonicKeys);
 
@@ -126,7 +144,11 @@ export class MetaMask {
       "button",
       "Don't enable enhanced protection",
     );
-    await clickElement(this.driver, XPATH_BTN_NO_ENCHANCED_PROTECTION);
+
+    if (await isDisplayed(this.driver, XPATH_BTN_NO_ENCHANCED_PROTECTION)) {
+      await clickElement(this.driver, XPATH_BTN_NO_ENCHANCED_PROTECTION);
+    }
+
     await this.acceptTNC();
     await this.skipPopup();
 
@@ -148,11 +170,12 @@ export class MetaMask {
     const XPATH_FIRST_WORD = buildDataTestIdXpath(IMPUT_MNEMONIC_FIELD + 0);
     await waitForElement(this.driver, XPATH_FIRST_WORD);
 
-    try {
-      await this.closeAnyExtraWindow(this.driver);
-    } catch (e) {
-      //no window to close
-    }
+    // not needed anymore
+    // try {
+    //   await this.closeAnyExtraWindow(this.driver);
+    // } catch (e) {
+    //   //no window to close
+    // }
 
     await this.fillPassPhrase(mnemonicKeys);
 
@@ -185,7 +208,11 @@ export class MetaMask {
       "button",
       "Don't enable enhanced protection",
     );
-    await clickElement(this.driver, XPATH_BTN_NO_ENCHANCED_PROTECTION);
+
+    if (await isDisplayed(this.driver, XPATH_BTN_NO_ENCHANCED_PROTECTION)) {
+      await clickElement(this.driver, XPATH_BTN_NO_ENCHANCED_PROTECTION);
+    }
+
     await this.acceptTNC();
     await this.skipPopup();
 
@@ -219,8 +246,7 @@ export class MetaMask {
   }
 
   async getAddress(): Promise<string> {
-    const XPATH_BTN_COPY_ADDRESS = buildDataTestIdXpath(BTN_COPY_ADDRESS);
-    return await getText(this.driver, XPATH_BTN_COPY_ADDRESS);
+    return await getText(this.driver, "//*[@class='qr-code']/*[2]");
   }
 
   async openAccountDetails() {
@@ -339,12 +365,20 @@ export class MetaMask {
     const XPATH_CANCEL_BTN = await buildXpathByElementText("button", "Cancel");
     //handle different cancel flows
     try {
-      await waitForElement(driver, XPATH_CANCEL_BTN);
+      await waitForElement(driver, XPATH_CANCEL_BTN, 10000);
       await clickElement(driver, XPATH_CANCEL_BTN);
     } catch (e) {
-      await waitForElement(driver, XPATH_BTN_REJECT_TRANSACTION);
+      await waitForElement(driver, XPATH_BTN_REJECT_TRANSACTION, 10000);
       await clickElement(driver, XPATH_BTN_REJECT_TRANSACTION);
     }
+  }
+
+  private static async rejectDeposit(driver: WebDriver) {
+    const XPATH_BTN_REJECT_TRANSACTION = buildDataTestIdXpath(
+      BTN_REJECT_TRANSACTION,
+    );
+    await waitForElement(driver, XPATH_BTN_REJECT_TRANSACTION, 10000);
+    await clickElement(driver, XPATH_BTN_REJECT_TRANSACTION);
   }
 
   async closeAnyExtraWindow(driver: WebDriver) {
@@ -389,6 +423,10 @@ export class MetaMask {
 
   static async rejectTransactionInDifferentWindow(driver: WebDriver) {
     await doActionInDifferentWindow(driver, this.rejectTransaction);
+  }
+
+  static async rejectDepositInDifferentWindow(driver: WebDriver) {
+    await doActionInDifferentWindow(driver, this.rejectDeposit);
   }
 
   static async signDepositInDifferentWindow(driver: WebDriver) {
