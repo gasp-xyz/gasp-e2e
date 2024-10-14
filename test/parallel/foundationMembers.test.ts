efb31fe97a5b4032a91f1f91e55aa64b1482eb26
AlexChetverov <alexchetverov@gmail.com>
2024-10-14 15:54:17 +0200

    added new tests and a new class

--- test/parallel/foundationMembers.test.ts
+++ test/parallel/foundationMembers.test.ts
@@ -1,4 +1,3 @@
-import { ApiPromise } from "@polkadot/api";
 import { getApi, initApi } from "../../utils/api";
 import { setupApi, setupUsers } from "../../utils/setup";
 import { Sudo } from "../../utils/sudo";
@@ -11,8 +10,8 @@ import {
 import { ExtrinsicResult } from "../../utils/eventListeners";
 import { BN_HUNDRED, BN_THOUSAND } from "gasp-sdk";
 import { Council } from "../../utils/Council";
+import { FoundationMembers } from "../../utils/FoundationMembers";
 
-let api: ApiPromise;
 let testUser: User;
 let foundationAddress: any;
 
@@ -22,36 +21,30 @@ beforeAll(async () => {
   } catch (e) {
     await initApi();
   }
-
-  api = getApi();
   await setupApi();
 });
 
 beforeEach(async () => {
   [testUser] = setupUsers();
   await Sudo.batchAsSudoFinalized(Assets.mintNative(testUser));
-  foundationAddress = JSON.parse(
-    JSON.stringify(await api.query.foundationMembers.members()),
-  )[0];
+  foundationAddress = (await FoundationMembers.getFoundationMembers())[0];
 });
 
 test("A founder can update his own foundation address", async () => {
   await Sudo.asSudoFinalized(
     Sudo.sudoAsWithAddressString(
       foundationAddress,
-      api.tx.foundationMembers.changeKey(testUser.keyRingPair.address),
+      FoundationMembers.changeKey(testUser),
     ),
   ).then((events) => {
     const res = getEventResultFromMangataTx(events);
     expect(res.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
   });
-  const foundationMembers = JSON.parse(
-    JSON.stringify(await api.query.foundationMembers.members()),
-  );
+  const foundationMembers = await FoundationMembers.getFoundationMembers();
   expect(foundationMembers).toContain(testUser.keyRingPair.address);
 });
 
-test("Council can not execute this extrinsic ( but with sudo.sudoAs )", async () => {
+test("Council can not execute this extrinsic", async () => {
   const [councilUser] = await setupUsers();
   await Sudo.batchAsSudoFinalized(
     Assets.mintNative(
@@ -61,11 +54,18 @@ test("Council can not execute this extrinsic ( but with sudo.sudoAs )", async ()
     Sudo.sudo(Council.setMembers([councilUser])),
   );
   const events = await Sudo.asSudoFinalized(
-    Sudo.sudoAs(
-      councilUser,
-      api.tx.foundationMembers.changeKey(testUser.keyRingPair.address),
-    ),
+    Sudo.sudoAs(councilUser, FoundationMembers.changeKey(testUser)),
   );
   const error = await getEventErrorFromSudo(events);
   expect(error.data).toEqual("NotMember");
+});
+
+test("Extrinsic must fail if sudo request any foundation modification", async () => {
+  await Sudo.batchAsSudoFinalized(FoundationMembers.changeKey(testUser)).then(
+    (events) => {
+      const res = getEventResultFromMangataTx(events);
+      expect(res.state).toEqual(ExtrinsicResult.ExtrinsicFailed);
+      expect(res.data).toEqual("NotMember");
+    },
+  );
 });
