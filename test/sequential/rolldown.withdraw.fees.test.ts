f7a55a2b4c6eb5b9e7e39fee23a7c7b327c559a5
AlexChetverov <alexchetverov@gmail.com>
2024-10-22 11:37:46 +0200

    fix issue

--- test/sequential/rolldown.withdraw.fees.test.ts
+++ test/sequential/rolldown.withdraw.fees.test.ts
@@ -15,7 +15,7 @@ import { Withdraw } from "../../utils/rolldown";
 import { BN, BN_MILLION } from "@polkadot/util";
 import {
   ExtrinsicResult,
-  filterZeroEventData,
+  filterAndStringifyFirstEvent,
 } from "../../utils/eventListeners";
 import { getEventResultFromMangataTx } from "../../utils/txHandler";
 import { stringToBN } from "../../utils/utils";
@@ -24,11 +24,7 @@ import { getTokensAccountInfo } from "../../utils/tx";
 let api: ApiPromise;
 let testUser: User;
 let gaspIdL1Asset: any;
-let withdrawalAmount: BN;
-let treasuryAccount: string;
 let ethIdL1Asset: any;
-let DEFAULT_AMOUNT: any;
-let chain: any;
 
 beforeAll(async () => {
   try {
@@ -44,10 +40,6 @@ beforeAll(async () => {
   ethIdL1Asset = JSON.parse(
     JSON.stringify(await api.query.assetRegistry.idToL1Asset(ETH_ASSET_ID)),
   );
-  withdrawalAmount = BN_MILLION;
-  treasuryAccount = "0x6d6f646c70792f74727372790000000000000000";
-  DEFAULT_AMOUNT = BN_THOUSAND.mul(BN_TEN.pow(new BN(18)));
-  chain = "Ethereum";
 });
 
 beforeEach(async () => {
@@ -55,6 +47,8 @@ beforeEach(async () => {
 });
 
 test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing GASP, some fees goes to treasury", async () => {
+  const treasuryAccount = "0x6d6f646c70792f74727372790000000000000000";
+  const chain = "Ethereum";
   const treasuryBalanceBefore = stringToBN(
     (await getTokensAccountInfo(treasuryAccount, GASP_ASSET_ID)).free,
   );
@@ -66,18 +60,21 @@ test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing GASP, some fees 
 
   const events = await signTx(
     getApi(),
-    await Withdraw(testUser, withdrawalAmount, gaspIdL1Asset.ethereum, chain),
+    await Withdraw(testUser, BN_MILLION, gaspIdL1Asset.ethereum, chain),
     testUser.keyRingPair,
   );
   const eventResponse = getEventResultFromMangataTx(events);
   expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
 
-  const transferEvent = filterZeroEventData(events, "Transfer");
+  const transferEvent = filterAndStringifyFirstEvent(events, "Transfer");
   const withdrawalFee = stringToBN(transferEvent.amount);
   expect(transferEvent.from).toEqual(testUser.keyRingPair.address);
   expect(transferEvent.to).toEqual(treasuryAccount);
-  const depositEvent = filterZeroEventData(events, "Deposited");
-  const networkFees = stringToBN(depositEvent.amount);
+  const transactionEvent = filterAndStringifyFirstEvent(
+    events,
+    "TransactionFeePaid",
+  );
+  const transactionFee = stringToBN(transactionEvent.actualFee);
 
   const treasuryBalanceAfter = stringToBN(
     (await getTokensAccountInfo(treasuryAccount, GASP_ASSET_ID)).free,
@@ -90,17 +87,20 @@ test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing GASP, some fees 
     treasuryBalanceBefore.add(withdrawalFee),
   );
   expect(userBalanceBefore).bnEqual(
-    userBalanceAfter.add(withdrawalFee).add(withdrawalAmount).add(networkFees),
+    userBalanceAfter.add(withdrawalFee).add(BN_MILLION).add(transactionFee),
   );
 });
 
 test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing Eth, some fees goes to treasury", async () => {
+  const treasuryAccount = "0x6d6f646c70792f74727372790000000000000000";
+  const mintingAmount = BN_THOUSAND.mul(BN_TEN.pow(new BN(18)));
+  const chain = "Ethereum";
   const treasuryBalanceBefore = stringToBN(
     (await getTokensAccountInfo(treasuryAccount, GASP_ASSET_ID)).free,
   );
   await Sudo.batchAsSudoFinalized(
     Assets.mintNative(testUser),
-    Assets.mintToken(ETH_ASSET_ID, testUser, DEFAULT_AMOUNT),
+    Assets.mintToken(ETH_ASSET_ID, testUser, mintingAmount),
   );
   const userGaspBalanceBefore = stringToBN(
     (await getTokensAccountInfo(testUser.keyRingPair.address, GASP_ASSET_ID))
@@ -113,18 +113,21 @@ test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing Eth, some fees g
 
   const events = await signTx(
     getApi(),
-    await Withdraw(testUser, withdrawalAmount, ethIdL1Asset.ethereum, chain),
+    await Withdraw(testUser, BN_MILLION, ethIdL1Asset.ethereum, chain),
     testUser.keyRingPair,
   );
   const eventResponse = getEventResultFromMangataTx(events);
   expect(eventResponse.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
 
-  const transferEvent = filterZeroEventData(events, "Transfer");
+  const transferEvent = filterAndStringifyFirstEvent(events, "Transfer");
   const withdrawalFee = stringToBN(transferEvent.amount);
   expect(transferEvent.from).toEqual(testUser.keyRingPair.address);
   expect(transferEvent.to).toEqual(treasuryAccount);
-  const depositEvent = filterZeroEventData(events, "Deposited");
-  const networkFees = stringToBN(depositEvent.amount);
+  const transactionEvent = filterAndStringifyFirstEvent(
+    events,
+    "TransactionFeePaid",
+  );
+  const transactionFee = stringToBN(transactionEvent.actualFee);
 
   const treasuryBalanceAfter = stringToBN(
     (await getTokensAccountInfo(treasuryAccount, GASP_ASSET_ID)).free,
@@ -141,14 +144,13 @@ test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing Eth, some fees g
     treasuryBalanceBefore.add(withdrawalFee),
   );
   expect(userGaspBalanceBefore).bnEqual(
-    userGaspBalanceAfter.add(withdrawalFee).add(networkFees),
+    userGaspBalanceAfter.add(withdrawalFee).add(transactionFee),
   );
-  expect(userEthBalanceBefore).bnEqual(
-    userEthBalanceAfter.add(withdrawalAmount),
-  );
+  expect(userEthBalanceBefore).bnEqual(userEthBalanceAfter.add(BN_MILLION));
 });
 
 test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing ALL GASP, extrinsic fail", async () => {
+  const chain = "Ethereum";
   await Sudo.batchAsSudoFinalized(Assets.mintNative(testUser));
   const userBalanceBefore = stringToBN(
     (await getTokensAccountInfo(testUser.keyRingPair.address, GASP_ASSET_ID))
@@ -166,18 +168,23 @@ test("GIVEN a withdrawal, WHEN paying with GASP and withdrawing ALL GASP, extrin
     (await getTokensAccountInfo(testUser.keyRingPair.address, GASP_ASSET_ID))
       .free,
   );
-  const depositEvent = filterZeroEventData(events, "Deposited");
-  const networkFees = stringToBN(depositEvent.amount);
-  expect(userBalanceBefore).bnEqual(userBalanceAfter.add(networkFees));
+  const transactionEvent = filterAndStringifyFirstEvent(
+    events,
+    "TransactionFeePaid",
+  );
+  const transactionFee = stringToBN(transactionEvent.actualFee);
+  expect(userBalanceBefore).bnEqual(userBalanceAfter.add(transactionFee));
 });
 
 test("GIVEN a withdrawal, WHEN user having only Eth and withdrawing Eth, extrinsic fail", async () => {
+  const chain = "Ethereum";
+  const mintingAmount = BN_THOUSAND.mul(BN_TEN.pow(new BN(18)));
   await Sudo.batchAsSudoFinalized(
-    Assets.mintToken(ETH_ASSET_ID, testUser, DEFAULT_AMOUNT),
+    Assets.mintToken(ETH_ASSET_ID, testUser, mintingAmount),
   );
   await signTx(
     getApi(),
-    await Withdraw(testUser, withdrawalAmount, ethIdL1Asset.ethereum, chain),
+    await Withdraw(testUser, BN_MILLION, ethIdL1Asset.ethereum, chain),
     testUser.keyRingPair,
   ).then((result) => {
     const eventResponse = getEventResultFromMangataTx(result);
@@ -186,7 +193,9 @@ test("GIVEN a withdrawal, WHEN user having only Eth and withdrawing Eth, extrins
   });
 });
 
-test("Given a fee withdrawal payment, tokens go to Treasury", async () => {
+test("Given a fee withdrawal payment WHEN withdrawal is sent from another account THEN the fee is deducted from from sender's account", async () => {
+  const chain = "Ethereum";
+  const treasuryAccount = "0x6d6f646c70792f74727372790000000000000000";
   const [testUser2] = setupUsers();
   await Sudo.batchAsSudoFinalized(Assets.mintNative(testUser2));
   const treasuryBalanceBefore = stringToBN(
@@ -198,13 +207,16 @@ test("Given a fee withdrawal payment, tokens go to Treasury", async () => {
   );
   const events = await signTx(
     getApi(),
-    await Withdraw(testUser, withdrawalAmount, gaspIdL1Asset.ethereum, chain),
+    await Withdraw(testUser, BN_MILLION, gaspIdL1Asset.ethereum, chain),
     testUser2.keyRingPair,
   );
-  const transferEvent = filterZeroEventData(events, "Transfer");
+  const transferEvent = filterAndStringifyFirstEvent(events, "Transfer");
   const withdrawalFee = stringToBN(transferEvent.amount);
-  const depositEvent = filterZeroEventData(events, "Deposited");
-  const networkFees = stringToBN(depositEvent.amount);
+  const transactionEvent = filterAndStringifyFirstEvent(
+    events,
+    "TransactionFeePaid",
+  );
+  const transactionFee = stringToBN(transactionEvent.actualFee);
   expect(transferEvent.from).toEqual(testUser2.keyRingPair.address);
   expect(transferEvent.to).toEqual(treasuryAccount);
 
@@ -221,7 +233,7 @@ test("Given a fee withdrawal payment, tokens go to Treasury", async () => {
   expect(testUser2BalanceBefore).bnEqual(
     testUser2BalanceAfter
       .add(withdrawalFee)
-      .add(withdrawalAmount)
-      .add(networkFees),
+      .add(BN_MILLION)
+      .add(transactionFee),
   );
 });
