a5a5c4e64584cd2116978b7e665d6a5a55d0989d
AlexChetverov <alexchetverov@gmail.com>
2025-01-15 13:25:31 +0100

    add tests

---
+++ test/sequential/council-pallet.API.sudo.test.ts
@@ -0,0 +1,179 @@
+import { jest } from "@jest/globals";
+import { BN_HUNDRED, BN_THOUSAND, signTx } from "gasp-sdk";
+import { getApi, initApi } from "../../utils/api";
+import { Assets } from "../../utils/Assets";
+import { getSudoUser, setupApi, setupUsers } from "../../utils/setup";
+import { Sudo } from "../../utils/sudo";
+import { User } from "../../utils/User";
+import { Council } from "../../utils/Council";
+import { ApiPromise } from "@polkadot/api";
+import { GASP_ASSET_ID } from "../../utils/Constants";
+import { FoundationMembers } from "../../utils/FoundationMembers";
+import { getEventResultFromMangataTx } from "../../utils/txHandler";
+import { ExtrinsicResult } from "../../utils/eventListeners";
+
+jest.spyOn(console, "log").mockImplementation(jest.fn());
+jest.setTimeout(2500000);
+process.env.NODE_ENV = "test";
+
+let councilUsers: User[];
+let sudo: User;
+let testUser: User;
+let api: ApiPromise;
+
+beforeAll(async () => {
+  try {
+    getApi();
+  } catch (e) {
+    await initApi();
+  }
+  await setupApi();
+
+  sudo = getSudoUser();
+  api = getApi();
+  councilUsers = await setupUsers();
+  [testUser] = await setupUsers();
+
+  councilUsers.push(sudo);
+  await Sudo.batchAsSudoFinalized(
+    Assets.mintNative(
+      councilUsers[0],
+      BN_HUNDRED.mul(BN_THOUSAND).mul(Assets.MG_UNIT),
+    ),
+    Assets.mintNative(
+      councilUsers[1],
+      BN_HUNDRED.mul(BN_THOUSAND).mul(Assets.MG_UNIT),
+    ),
+    Assets.mintNative(
+      councilUsers[2],
+      BN_HUNDRED.mul(BN_THOUSAND).mul(Assets.MG_UNIT),
+    ),
+    Sudo.sudo(
+      Council.setMembers([councilUsers[0], councilUsers[1], councilUsers[2]]),
+    ),
+  );
+});
+
+test("temp", async () => {
+  const propEvents = await Sudo.asSudoFinalized(
+    Sudo.sudoAs(
+      councilUsers[0],
+      Sudo.batch(
+        Council.propose(
+          3,
+          api.tx.tokens.mint(
+            GASP_ASSET_ID,
+            testUser.keyRingPair.address,
+            BN_THOUSAND,
+          ),
+          44,
+        ),
+      ),
+    ),
+  );
+  const hash = propEvents
+    .filter((x) => x.event.method === "Proposed")
+    .flatMap((x) => x.eventData[2].data.toString());
+  await Council.voteProposal(hash[0], [
+    councilUsers[0],
+    councilUsers[1],
+    councilUsers[2],
+  ]);
+
+  const propIndex = JSON.parse(
+    JSON.stringify(await api.query.council.voting(hash[0])),
+  ).index;
+  const foundationMembers = await FoundationMembers.getFoundationMembers();
+  await Sudo.asSudoFinalized(
+    Sudo.sudoAsWithAddressString(
+      foundationMembers[1],
+      Council.close(hash[0], propIndex),
+    ),
+  );
+});
+
+test("temp2", async () => {
+  const propEvents = await signTx(
+    api,
+    Council.propose(
+      3,
+      api.tx.sudoOrigin.sudo(
+        api.tx.tokens.mint(
+          GASP_ASSET_ID,
+          testUser.keyRingPair.address,
+          BN_THOUSAND,
+        ),
+      ),
+      44,
+    ),
+    councilUsers[0].keyRingPair,
+  );
+
+  const hash = propEvents
+    .filter((x) => x.event.method === "Proposed")
+    .flatMap((x) => x.eventData[2].data.toString());
+
+  const proposal = await api.query.council.voting(hash[0]);
+  for (let i = 0; i < 3; i++) {
+    const tx = Council.vote(
+      hash[0],
+      JSON.parse(JSON.stringify(proposal.toHuman())).index,
+      "aye",
+    );
+    await signTx(api, tx, councilUsers[i].keyRingPair);
+  }
+});
+
+test("temp3", async () => {
+  const [newFounder] = await setupUsers();
+
+  const propEvents = await Sudo.asSudoFinalized(
+    Sudo.sudoAs(
+      councilUsers[0],
+      Sudo.batch(
+        Council.propose(
+          3,
+          api.tx.tokens.mint(
+            GASP_ASSET_ID,
+            testUser.keyRingPair.address,
+            BN_THOUSAND,
+          ),
+          44,
+        ),
+      ),
+    ),
+  );
+  const hash = propEvents
+    .filter((x) => x.event.method === "Proposed")
+    .flatMap((x) => x.eventData[2].data.toString());
+  await Council.voteProposal(hash[0], [
+    councilUsers[0],
+    councilUsers[1],
+    councilUsers[2],
+  ]);
+
+  const propIndex = JSON.parse(
+    JSON.stringify(await api.query.council.voting(hash[0])),
+  ).index;
+
+  const foundationMembers = await FoundationMembers.getFoundationMembers();
+  await Sudo.asSudoFinalized(
+    Sudo.sudoAsWithAddressString(
+      foundationMembers[2],
+      api.tx.foundationMembers.changeKey(newFounder.keyRingPair.address),
+    ),
+  ).then((events) => {
+    const res = getEventResultFromMangataTx(events);
+    expect(res.state).toEqual(ExtrinsicResult.ExtrinsicSuccess);
+  });
+
+  await Sudo.batchAsSudoFinalized(
+    Assets.mintNative(
+      newFounder,
+      BN_HUNDRED.mul(BN_THOUSAND).mul(Assets.MG_UNIT),
+    ),
+    Sudo.sudo(api.tx.sudo.removeKey()),
+  );
+
+  await signTx(api, Council.close(hash[0], propIndex), newFounder.keyRingPair);
+});
