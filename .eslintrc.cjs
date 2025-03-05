e9ddacbe9568a6a9c8c7f1910900fae748bc8c0a
tenequm <tenequm@gmail.com>
2025-03-05 22:04:51 +0000

    chore: upgrade ESLint and TypeScript dependencies to latest versions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,16 @@
 module.exports = {
-  extends: ['react-app', 'react-app/jest', 'prettier'],
+  extends: ['prettier'],
   globals: {},
+  settings: {
+    react: {
+      version: '999.999.999',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
   rules: {
     'no-var': 'warn',
     'prefer-const': 'warn',
@@ -11,11 +21,21 @@ module.exports = {
     'curly': 'warn',
     "testing-library/no-debugging-utils" : 'off',
   },
-  plugins: ["prettier"],
+  plugins: ["prettier", "react", "jest", "import"],
   overrides: [
     {
       files: ['**/*.ts?(x)'],
       rules: {},
     },
   ],
+  env: {
+    "jest/globals": true,
+    "node": true,
+    "es6": true
+  },
+  settings: {
+    "import/resolver": {
+      "typescript": {}
+    }
+  },
 };
