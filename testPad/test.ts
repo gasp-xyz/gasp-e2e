65e6f7dc9fd87e5e1fdb74dc5deb4ee70beb10e8
goncer <gonza.cerro@gmail.com>
2025-03-11 17:43:10 +0100

    Update test.ts
    dummy change to trigger a full run.

--- testPad/test.ts
+++ testPad/test.ts
@@ -2,9 +2,6 @@ import { getApi, initApi } from "../utils/api";
 import "dotenv/config";
 import { jest } from "@jest/globals";
 
-jest.spyOn(console, "log").mockImplementation(jest.fn());
-
-jest.setTimeout(1500000);
 process.env.NODE_ENV = "test";
 
 describe("L1 update", () => {
