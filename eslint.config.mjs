33bf8a58760f66e4a257d088fab722297ca2b0e4
GonCer <gonza.cerro@gmail.com>
2025-03-19 13:06:38 +0100

    chore: skip contracts.sol folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default [
       "test/exploratory/**", 
       "**/reports/**", 
       "**/.yarn/**",
+      "**/contracts.sol/**",
       "utils/globalSetup.js",
       "utils/globalTeardown.js"
     ],
