89f622d5b29d46a65ae151b4c041058a492bf2b5
Gonzalo Del Cerro <gonzalodelcerro@Gonzalos-MacBook-Pro.local>
2025-03-25 12:36:24 +0100

    chore: reported issue on buy

--- test/parallel/market-pallet.stablePool.test.ts
+++ test/parallel/market-pallet.stablePool.test.ts
@@ -86,6 +86,7 @@ test("Happy path - create stable pool", async () => {
   expect(poolId).bnGt(BN_ZERO);
 });
 
+// Goncer: Investigated & Reported.
 test("Happy path - Swap tokens in stableSwap pool", async () => {
   const event = await signTx(
     api,
